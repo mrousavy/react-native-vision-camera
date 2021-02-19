763b4d537297eb4786280f8b9ba22863cacbba79
Marc Rousavy <marcrousavy@hotmail.com>
2021-02-19 16:20:19 +0100

    Finish devDependencies setup

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	bracketSpacing: true,
+	jsxBracketSameLine: true,
+	singleQuote: true,
+	trailingComma: 'all',
+	semi: true,
+	tabWidth: 2,
+	useTabs: false,
+	printWidth: 150
+};
