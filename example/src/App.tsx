f913fc11ca0a0b334c8b0d910db31b7f9997088f
Marc Rousavy <marcrousavy@hotmail.com>
2021-02-19 16:44:00 +0100

    Clean up example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,19 +1,12 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import VisionCamera from 'react-native-vision-camera';
-
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    VisionCamera.multiply(3, 7).then(setResult);
-  }, []);
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text>Hello World!</Text>
     </View>
   );
 }
