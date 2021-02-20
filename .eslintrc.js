49f0169ee40d72d29a16358e811de100e2f99529
Marc Rousavy <marcrousavy@hotmail.com>
2021-02-20 17:09:57 +0100

    Fix include

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
-    project: ["./tsconfig.json"],
+    project: ['./tsconfig.json'],
     ecmaFeatures: {
       jsx: true,
     },
