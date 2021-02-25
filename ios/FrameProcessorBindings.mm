ec5cd411d9604dd2b861b2645dc79fe89f0a65bd
Marc Rousavy <marcrousavy@hotmail.com>
2021-02-25 20:36:25 +0100

    Install first binding

--- ios/FrameProcessorBindings.mm
+++ ios/FrameProcessorBindings.mm
@@ -21,7 +21,21 @@ using namespace facebook;
   if (!cxxBridge.runtime) {
     return;
   }
-  jsi::Runtime& runtime = *(jsi::Runtime*)cxxBridge.runtime;
+  jsi::Runtime& jsiRuntime = *(jsi::Runtime*)cxxBridge.runtime;
+  
+  
+  // setFrameProcessor(viewTag: number, frameProcessor: (frame: Frame) => void)
+  auto setFrameProcessor = jsi::Function::createFromHostFunction(jsiRuntime,
+                                                                 jsi::PropNameID::forAscii(jsiRuntime, "setFrameProcessor"),
+                                                                 2,  // viewTag, frameProcessor
+                                                                 [](jsi::Runtime& runtime, const jsi::Value& thisValue, const jsi::Value* arguments, size_t count) -> jsi::Value {
+    if (!arguments[0].isNumber()) throw jsi::JSError(runtime, "Camera::setFrameProcessor: First argument ('viewTag') must be a number!");
+    if (!arguments[1].isObject()) throw jsi::JSError(runtime, "Camera::setFrameProcessor: Second argument ('frameProcessor') must be a function!");
+    auto viewTag = arguments[0].getNumber();
+    
+    return jsi::Value::undefined();
+  });
+  jsiRuntime.global().setProperty(jsiRuntime, "setFrameProcessor", std::move(setFrameProcessor));
   
 }
 
