fb61c3775cf92102d77a8b3ecd4ebfdbfe76dba4
Marc Rousavy <marcrousavy@hotmail.com>
2021-02-26 19:44:22 +0100

    Export frame processor funcs

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,16 @@ export * from './CameraDevice';
 export * from './CameraError';
 export * from './CameraPosition';
 export * from './CameraPreset';
-<<<<<<< HEAD
 export * from './CameraProps';
-=======
->>>>>>> Remove onCodeScanned and all references
+export * from './FrameProcessor';
 export * from './PhotoFile';
 export * from './Point';
 export * from './Snapshot';
 export * from './TemporaryFile';
 export * from './VideoFile';
+
 export * from './hooks/useCameraDevices';
 export * from './hooks/useCameraFormat';
+export * from './hooks/useFrameProcessor';
+
 export * from './utils/FormatFilter';
