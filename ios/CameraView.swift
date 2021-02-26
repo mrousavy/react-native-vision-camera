60bb4a706da955f2990a94b29cec353668f6596e
Marc Rousavy <marcrousavy@hotmail.com>
2021-02-26 18:09:34 +0100

    Remove Metadata Object/Barcode scanning

--- ios/CameraView.swift
+++ ios/CameraView.swift
@@ -207,6 +207,216 @@ public final class CameraView: UIView {
     }
   }
 
+<<<<<<< HEAD
+=======
+  // pragma MARK: Session, Device and Format Configuration
+  /**
+   Configures the Capture Session.
+   */
+  private final func configureCaptureSession() {
+    isReady = false
+
+    #if targetEnvironment(simulator)
+    return invokeOnError(.device(.notAvailableOnSimulator))
+    #endif
+
+    guard cameraId != nil else {
+      return invokeOnError(.device(.noDevice))
+    }
+    let cameraId = self.cameraId! as String
+
+    ReactLogger.log(level: .info, message: "Initializing Camera with device \(cameraId)...")
+    captureSession.beginConfiguration()
+    defer {
+      captureSession.commitConfiguration()
+    }
+
+    if let preset = self.preset {
+      var sessionPreset: AVCaptureSession.Preset?
+      do {
+        sessionPreset = try AVCaptureSession.Preset(withString: preset)
+      } catch let EnumParserError.unsupportedOS(supportedOnOS: os) {
+        return invokeOnError(.parameter(.unsupportedOS(unionName: "Preset", receivedValue: preset, supportedOnOs: os)))
+      } catch {
+        return invokeOnError(.parameter(.invalid(unionName: "Preset", receivedValue: preset)))
+      }
+      if sessionPreset != nil {
+        if captureSession.canSetSessionPreset(sessionPreset!) {
+          captureSession.sessionPreset = sessionPreset!
+        } else {
+          // non-fatal error, so continue with configuration
+          invokeOnError(.format(.invalidPreset(preset: preset)))
+        }
+      }
+    }
+
+    // INPUTS
+    // Audio Setup
+    do {
+      captureSession.automaticallyConfiguresApplicationAudioSession = false
+      try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playAndRecord, options: [.mixWithOthers, .allowBluetooth, .defaultToSpeaker])
+      try AVAudioSession.sharedInstance().setActive(true)
+    } catch let error as NSError {
+      // not critical, so don't return
+      invokeOnError(.session(.audioSessionSetupFailed(reason: error.description)))
+      captureSession.automaticallyConfiguresApplicationAudioSession = true // fallback to auto-setup
+    }
+
+    // Video Input
+    do {
+      if let videoDeviceInput = self.videoDeviceInput {
+        captureSession.removeInput(videoDeviceInput)
+      }
+      guard let videoDevice = AVCaptureDevice(uniqueID: cameraId) else {
+        return invokeOnError(.device(.invalid))
+      }
+      zoom = NSNumber(value: Double(videoDevice.neutralZoomPercent))
+      videoDeviceInput = try AVCaptureDeviceInput(device: videoDevice)
+      guard captureSession.canAddInput(videoDeviceInput!) else {
+        return invokeOnError(.parameter(.unsupportedInput(inputDescriptor: "video-input")))
+      }
+      captureSession.addInput(videoDeviceInput!)
+    } catch {
+      return invokeOnError(.device(.invalid))
+    }
+
+    // Microphone (Audio Input)
+    do {
+      if let audioDeviceInput = self.audioDeviceInput {
+        captureSession.removeInput(audioDeviceInput)
+      }
+      guard let audioDevice = AVCaptureDevice.default(for: .audio) else {
+        return invokeOnError(.device(.microphoneUnavailable))
+      }
+
+      audioDeviceInput = try AVCaptureDeviceInput(device: audioDevice)
+      guard captureSession.canAddInput(audioDeviceInput!) else {
+        return invokeOnError(.parameter(.unsupportedInput(inputDescriptor: "audio-input")))
+      }
+      captureSession.addInput(audioDeviceInput!)
+    } catch {
+      return invokeOnError(.device(.invalid))
+    }
+
+    // OUTPUTS
+    if let photoOutput = self.photoOutput {
+      captureSession.removeOutput(photoOutput)
+    }
+    // Photo Output
+    photoOutput = AVCapturePhotoOutput()
+    photoOutput!.isDepthDataDeliveryEnabled = photoOutput!.isDepthDataDeliverySupported && enableDepthData
+    if let enableHighResolutionCapture = self.enableHighResolutionCapture?.boolValue {
+      photoOutput!.isHighResolutionCaptureEnabled = enableHighResolutionCapture
+    }
+    if #available(iOS 12.0, *) {
+      photoOutput!.isPortraitEffectsMatteDeliveryEnabled = photoOutput!.isPortraitEffectsMatteDeliverySupported && self.enablePortraitEffectsMatteDelivery
+    }
+    guard captureSession.canAddOutput(photoOutput!) else {
+      return invokeOnError(.parameter(.unsupportedOutput(outputDescriptor: "photo-output")))
+    }
+    captureSession.addOutput(photoOutput!)
+    if videoDeviceInput!.device.position == .front {
+        photoOutput!.mirror()
+    }
+
+    // Video Output
+    if let movieOutput = self.movieOutput {
+      captureSession.removeOutput(movieOutput)
+    }
+    movieOutput = AVCaptureMovieFileOutput()
+    guard captureSession.canAddOutput(movieOutput!) else {
+      return invokeOnError(.parameter(.unsupportedOutput(outputDescriptor: "movie-output")))
+    }
+    captureSession.addOutput(movieOutput!)
+    if videoDeviceInput!.device.position == .front {
+        movieOutput!.mirror()
+    }
+
+    ReactLogger.log(level: .info, message: "Camera initialized!")
+    invokeOnInitialized()
+    isReady = true
+  }
+
+  /**
+   Configures the Video Device to find the best matching Format.
+   */
+  private final func configureFormat() {
+    guard let filter = self.format else {
+      // Format Filter was null. Ignore it.
+      return
+    }
+    guard let device = videoDeviceInput?.device else {
+      return invokeOnError(.session(.cameraNotReady))
+    }
+
+    if device.activeFormat.matchesFilter(filter) {
+      ReactLogger.log(level: .info, message: "Active format already matches filter.")
+      return
+    }
+
+    // get matching format
+    let matchingFormats = device.formats.filter { $0.matchesFilter(filter) }.sorted { $0.isBetterThan($1) }
+    guard let format = matchingFormats.first else {
+      return invokeOnError(.format(.invalidFormat))
+    }
+
+    do {
+      try device.lockForConfiguration()
+      device.activeFormat = format
+      device.unlockForConfiguration()
+    } catch let error as NSError {
+      return invokeOnError(.device(.configureError), cause: error)
+    }
+  }
+
+  /**
+   Configures the Video Device with the given FPS, HDR and ColorSpace.
+   */
+  private final func configureDevice() {
+    guard let device = videoDeviceInput?.device else {
+      return invokeOnError(.session(.cameraNotReady))
+    }
+
+    do {
+      try device.lockForConfiguration()
+
+      if let fps = self.fps?.int32Value {
+        let duration = CMTimeMake(value: 1, timescale: fps)
+        device.activeVideoMinFrameDuration = duration
+        device.activeVideoMaxFrameDuration = duration
+      } else {
+        device.activeVideoMinFrameDuration = CMTime.invalid
+        device.activeVideoMaxFrameDuration = CMTime.invalid
+      }
+      if hdr != nil {
+        if hdr == true && !device.activeFormat.isVideoHDRSupported {
+          return invokeOnError(.format(.invalidHdr))
+        }
+        if !device.automaticallyAdjustsVideoHDREnabled {
+          if device.isVideoHDREnabled != hdr!.boolValue {
+            device.isVideoHDREnabled = hdr!.boolValue
+          }
+        }
+      }
+      if lowLightBoost != nil {
+        if lowLightBoost == true && !device.isLowLightBoostSupported {
+          return invokeOnError(.device(.lowLightBoostNotSupported))
+        }
+        if device.automaticallyEnablesLowLightBoostWhenAvailable != lowLightBoost!.boolValue {
+          device.automaticallyEnablesLowLightBoostWhenAvailable = lowLightBoost!.boolValue
+        }
+      }
+      if colorSpace != nil, let avColorSpace = try? AVCaptureColorSpace(string: String(colorSpace!)) {
+        device.activeColorSpace = avColorSpace
+      }
+
+      device.unlockForConfiguration()
+    } catch let error as NSError {
+      return invokeOnError(.device(.configureError), cause: error)
+    }
+  }
+
+>>>>>>> Remove Metadata Object/Barcode scanning
   internal final func setTorchMode(_ torchMode: String) {
     guard let device = videoDeviceInput?.device else {
       return invokeOnError(.session(.cameraNotReady))
--- ios/CameraViewManager.m
+++ ios/CameraViewManager.m
@@ -12,12 +12,6 @@
 #import <React/RCTViewManager.h>
 #import <React/RCTUtils.h>
 
-// In VisionCameraExample (VisionCamera):
-// #import <react-native-vision-camera/react_native_vision_camera-Swift.h>
-#import "react_native_vision_camera-Swift.h"
-// In VisionCamera:
-//#import <VisionCamera/VisionCamera-Swift.h>
-
 @interface RCT_EXTERN_REMAP_MODULE(CameraView, CameraViewManager, RCTViewManager)
 
 // Module Functions
