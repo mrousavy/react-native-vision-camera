c71c68d9b556773c6a511d88ddf3fd696f635cdf
Marc Rousavy <marcrousavy@hotmail.com>
2021-02-27 21:54:33 +0100

    Lazily initialize jsi::Runtime on DispatchQueue

--- ios/Frame Processor/FrameProcessorDelegate.mm
+++ ios/Frame Processor/FrameProcessorDelegate.mm
@@ -15,7 +15,14 @@
 #import "../../cpp/RuntimeDecorator.h"
 #import "../JSI Utils/YeetJSIUtils.h"
 
+#if !__has_include(<RNReanimated/NativeReanimatedModule.h>)
+#error The NativeReanimatedModule.h header could not be found, make sure you install react-native-reanimated!
+#endif
+
+#import <RNReanimated/ShareableValue.h>
+ 
 using namespace facebook;
+//using namespace reanimated;
 
 @implementation FrameProcessorDelegate {
   std::unique_ptr<jsi::Function> worklet;
@@ -31,8 +38,7 @@ using namespace facebook;
     dispatch_queue_attr_t qos = dispatch_queue_attr_make_with_qos_class(DISPATCH_QUEUE_SERIAL, QOS_CLASS_USER_INTERACTIVE, -1);
     dispatchQueue = dispatch_queue_create("com.mrousavy.camera-frame-processor", qos);
     // TODO: Do I need to create the JSI runtime in the `dispatchQueue`'s Thread?
-    runtime = std::unique_ptr<jsi::Runtime>(vision::makeJSIRuntime());
-    vision::RuntimeDecorator::decorateRuntime(*runtime);
+
   }
   return self;
 }
@@ -40,6 +46,10 @@ using namespace facebook;
 - (void) setFrameProcessorFunction:(void*)function {
   // TODO: Make sure this unique_ptr stuff works, because it seems like a very bad idea to move the jsi::Function and keep a strong reference
   worklet = std::unique_ptr<jsi::Function>(static_cast<jsi::Function*>(function));
+  
+  // TODO: Use ShareableValue::adapt without a NativeReanimatedModule instance?
+  // auto workletShareable = reanimated::ShareableValue::adapt(*runtime, *worklet, reanimated::ValueType::UndefinedType);
+  
   // TODO: Workletize the [worklet] using the Reanimated API
   //  1. Capture any variables/functions from "outside" the function (Copy over to this runtime & freeze)
   //  2. Make sure the user can assign Reanimated SharedValues in the worklet, no idea if this is already supported after step 1.
@@ -48,12 +58,19 @@ using namespace facebook;
 }
 
 - (void) captureOutput:(AVCaptureOutput *)output didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection {
+  vision::Logger::log("FrameProcessorDelegate: Camera frame arrived");
   if (!runtime) {
-    vision::Logger::log("FrameProcessorDelegate: Camera frame arrived, but JSI Runtime has not been created yet! This might indicate a memory leak.");
-    return;
+    vision::Logger::log("FrameProcessorDelegate: Creating JSI Runtime...");
+    auto start = std::chrono::system_clock::now();
+    runtime = std::unique_ptr<jsi::Runtime>(vision::makeJSIRuntime());
+    vision::RuntimeDecorator::decorateRuntime(*runtime);
+    auto end = std::chrono::system_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    std::string msg = "FrameProcessorDelegate: Runtime created! Took " + std::to_string(elapsed) + " seconds";
+    vision::Logger::log(msg.c_str());
   }
   // TODO: Call [worklet] with the actual frame output buffer
-  worklet->call(*runtime, convertNSStringToJSIString(*runtime, @"Hello from VisionCamera!"), 1);
+  worklet->callWithThis(*runtime, *worklet, convertNSStringToJSIString(*runtime, @"Hello from VisionCamera!"), 1);
 }
 
 @end
