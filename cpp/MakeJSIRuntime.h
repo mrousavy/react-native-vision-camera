be00ec356f0e6c92fdfda6f49ce74a766b6a4c76
Marc Rousavy <marcrousavy@hotmail.com>
2021-02-27 14:34:25 +0100

    Create MakeJSIRuntime

---
+++ cpp/MakeJSIRuntime.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <jsi/jsi.h>
+
+#if __has_include(<hermes/hermes.h>)
+#include <hermes/hermes.h>
+#else
+#include <jsi/JSCRuntime.h>
+#endif
+
+using namespace facebook;
+
+namespace vision {
+
+static std::unique_ptr<jsi::Runtime> makeJSIRuntime() {
+  #if __has_include(<hermes/hermes.h>)
+  return facebook::hermes::makeHermesRuntime();
+  #else
+  return facebook::jsc::makeJSCRuntime();
+  #endif
+}
+  
+}
