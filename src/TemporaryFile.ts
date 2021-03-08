b1f78c08077659a4123caecc0a9e0541fd02710e
Marc Rousavy <marcrousavy@hotmail.com>
2021-03-08 18:00:29 +0100

    Update TemporaryFile.ts

--- src/TemporaryFile.ts
+++ src/TemporaryFile.ts
@@ -3,7 +3,11 @@
  */
 export interface TemporaryFile {
   /**
-   * The path of the file. This file might get deleted once the app closes because it lives in the temp directory.
+   * The path of the file.
+   *
+   * * **Note:** If you want to consume this file (e.g. for displaying it in an `<Image>` component), you might have to add the `file://` prefix.
+   *
+   * * **Note:** This file might get deleted once the app closes because it lives in the temp directory.
    */
   path: string;
 }
