948a9e5ff0341100e89730f15ade6635f146aa36
Marc Rousavy <marcrousavy@hotmail.com>
2021-03-09 10:38:08 +0100

    Only run SwiftFormat in pre-commit

--- scripts/swiftlint.sh
+++ scripts/swiftlint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if which swiftlint >/dev/null; then
-  cd ios && swiftlint autocorrect && swiftlint
+  cd ios && swiftlint --fix && swiftlint
 else
   echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
 fi
