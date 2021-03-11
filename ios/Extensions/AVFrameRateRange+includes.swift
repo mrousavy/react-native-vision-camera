c8bf67edc979dfc34c2c5177d99bab9408e2dfe9
Marc Rousavy <marcrousavy@hotmail.com>
2021-03-11 19:47:51 +0100

    Update AVFrameRateRange+includes.swift

--- ios/Extensions/AVFrameRateRange+includes.swift
+++ ios/Extensions/AVFrameRateRange+includes.swift
@@ -9,6 +9,9 @@
 import AVFoundation
 
 extension AVFrameRateRange {
+  /**
+   * Returns true if this [AVFrameRateRange] contains the given [fps]
+   */
   func includes(fps: Double) -> Bool {
     return fps >= minFrameRate && fps <= maxFrameRate
   }
