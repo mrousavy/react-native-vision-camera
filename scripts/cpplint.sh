616cc6ed319c09ffe6c5f38a755c8f7991223243
Marc Rousavy <marcrousavy@hotmail.com>
2021-03-19 16:59:06 +0100

    Update cpplint.sh

--- scripts/cpplint.sh
+++ scripts/cpplint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if which cpplint >/dev/null; then
-  cd cpp && cpplint
+  cpplint --linelength=160 --recursive cpp android/src/main/cpp
 else
   echo "warning: cpplint not installed, download from https://github.com/cpplint/cpplint"
 fi
