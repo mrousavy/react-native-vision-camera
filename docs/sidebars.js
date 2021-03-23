8bcf2ae4e39cb978ff15548702d8355a35a92228
Marc Rousavy <marcrousavy@hotmail.com>
2021-03-23 15:08:43 +0100

    Rename sidebar

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,5 +1,5 @@
 module.exports = {
-  someSidebar: {
+  visionSidebar: {
     Guides: ['guides/setup', 'guides/devices', 'guides/formats', 'guides/capturing', 'guides/frame-processors', 'guides/animated', 'guides/errors', 'guides/troubleshooting'],
     API: require('./typedoc-sidebar.js'),
   },
