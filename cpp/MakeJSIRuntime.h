a6574dc94207c912ec94dc11a7bf3ee3cb9b1450
Marc Rousavy <marcrousavy@hotmail.com>
2021-03-31 16:12:21 +0200

    Add support for V8 runtime

--- cpp/MakeJSIRuntime.h
+++ cpp/MakeJSIRuntime.h
@@ -4,8 +4,13 @@
 #include <memory>
 
 #if __has_include(<hermes/hermes.h>)
+// Hermes (https://hermesengine.dev)
 #include <hermes/hermes.h>
+#elif __has_include(<v8runtime/V8RuntimeFactory.h>)
+// V8 (https://github.com/Kudo/react-native-v8)
+#include <v8runtime/V8RuntimeFactory.h>
 #else
+// JSC
 #include <jsi/JSCRuntime.h>
 #endif
 
@@ -16,6 +21,8 @@ namespace vision {
 static std::unique_ptr<jsi::Runtime> makeJSIRuntime() {
   #if __has_include(<hermes/hermes.h>)
   return facebook::hermes::makeHermesRuntime();
+  #elif __has_include(<v8runtime/V8RuntimeFactory.h>)
+  return facebook::createV8Runtime("");
   #else
   return facebook::jsc::makeJSCRuntime();
   #endif
