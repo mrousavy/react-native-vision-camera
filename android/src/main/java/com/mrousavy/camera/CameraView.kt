0c5f23af488d1020d3a0c9d7d18d6ae97e518889
Marc Rousavy <marcrousavy@hotmail.com>
2021-04-13 09:24:14 +0200

    Set scaleType=FIT_CENTER to avoid expensive takePhoto cropping

--- android/src/main/java/com/mrousavy/camera/CameraView.kt
+++ android/src/main/java/com/mrousavy/camera/CameraView.kt
@@ -107,6 +107,7 @@ class CameraView(context: Context) : FrameLayout(context), LifecycleOwner {
   init {
     previewView = PreviewView(context)
     previewView.layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+    previewView.scaleType = PreviewView.ScaleType.FIT_CENTER // avoids cropping on takePhoto
     previewView.installHierarchyFitter() // If this is not called correctly, view finder will be black/blank
     addView(previewView)
 
