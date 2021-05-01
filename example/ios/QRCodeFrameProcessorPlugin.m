d78548a6abfb2ee041c4d9349cae5f46df796311
Marc Rousavy <marcrousavy@hotmail.com>
2021-05-01 15:02:33 +0200

    Implement QR Code Frame Processor Plugin in Swift

--- example/ios/QRCodeFrameProcessorPlugin.m
+++ example/ios/QRCodeFrameProcessorPlugin.m
@@ -15,10 +15,11 @@
 @implementation QRFrameProcessorPlugin
 
 + (id) scanQRCodes:(CMSampleBufferRef)buffer {
-  NSLog(@"Called with buffer!");
-  return [NSArray arrayWithObjects:@"test"];
+  NSLog(@"scanQRCodes Objective-C frame processor called!");
+  return @[];
 }
 
-VISION_EXPORT_FRAME_PROCESSOR(scanQRCodes)
+// Uncomment this line to register the above function as a frame processor
+//VISION_EXPORT_FRAME_PROCESSOR(scanQRCodes)
 
 @end
--- ios/Frame Processor/FrameProcessorPlugin.h
+++ ios/Frame Processor/FrameProcessorPlugin.h
@@ -9,50 +9,55 @@
 #ifndef FrameProcessorPlugin_h
 #define FrameProcessorPlugin_h
 
+#import <Foundation/Foundation.h>
 #import "FrameProcessorPluginRegistry.h"
 #import <CoreMedia/CMSampleBuffer.h>
+
+@protocol FrameProcessorPluginBase
++ (id) callback:(CMSampleBufferRef)buffer;
+@end
+
 
 #define VISION_CONCAT2(A, B) A##B
 #define VISION_CONCAT(A, B) VISION_CONCAT2(A, B)
 
 /**
- * Place this macro in your class implementation to automatically register
- * your module with the bridge when it loads. The optional js_name argument
- * will be used as the JS module name. If omitted, the JS module name will
- * match the Objective-C class name.
+ * Use this Macro to register the given function as a Frame Processor.
+ * * Make sure the given function matches the following signature: (id)callback:(CMSampleBufferRef)
+ * * Make sure the given function's name is unique across other frame processor plugins
+ * * Make sure your frame processor returns a Value that can be converted to JS
+ * * Make sure to use this Macro in an @implementation, not @interface
  */
 #define VISION_EXPORT_FRAME_PROCESSOR(frame_processor)          \
 \
 +(void)load                                         \
 {                                                   \
-SEL selector = @selector(frame_processor:);         \
-IMP method = [self methodForSelector:selector];     \
-id (*func)(CMSampleBufferRef) = (void*) method;     \
-[FrameProcessorPluginRegistry addFrameProcessorPlugin:@"__" @ #frame_processor callback:^id(CMSampleBufferRef buffer) { \
-  return func(buffer);        \
-}];       \
+  SEL selector = @selector(frame_processor:);         \
+  IMP method = [self methodForSelector:selector];     \
+  id (*func)(CMSampleBufferRef) = (void*) method;     \
+  [FrameProcessorPluginRegistry addFrameProcessorPlugin:@"__" @ #frame_processor callback:^id(CMSampleBufferRef buffer) { \
+    return func(buffer);        \
+  }];       \
 }
 
+
 /**
- * Same as VISION_EXPORT_FRAME_PROCESSOR, but uses __attribute__((constructor)) for module
- * registration. Useful for registering swift classes that forbids use of load
- * Used in RCT_EXTERN_REMAP_MODULE
+ * Same as VISION_EXPORT_FRAME_PROCESSOR, but uses __attribute__((constructor)) for
+ * registration. Useful for registering swift classes that forbids use of +(void)load.
  */
-#define VISION_EXPORT_SWIFT_FRAME_PROCESSOR(name, objc_name, objc_supername) \
-objc_name:                                                        \
-objc_supername @                                                  \
-end @interface objc_name(FrameProcessorPlugin)        \
-@end                                                              \
-@implementation objc_name (FrameProcessorPlugin)                       \
-\
-  + (id) callback:(CMSampleBufferRef)buffer; \
-\
-  __attribute__((constructor)) static void VISION_CONCAT(initialize_, objc_name)() \
-  {                                                                             \
-    SEL selector = @selector(callback:);    \
-    [FrameProcessorPluginRegistry addFrameProcessorPlugin:@ #name callback:^id(CMSampleBufferRef buffer) { \
-      return [objc_name callback:buffer]; \
-    }]; \
-  }
+#define VISION_EXPORT_SWIFT_FRAME_PROCESSOR(name, objc_name) \
+objc_name : NSObject<FrameProcessorPluginBase>                                      \
+@end                                                                                \
+                                                                                    \
+@interface objc_name (FrameProcessorPlugin)                                         \
+@end                                                                                \
+@implementation objc_name (FrameProcessorPlugin)                                    \
+                                                                                    \
+__attribute__((constructor)) static void VISION_CONCAT(initialize_, objc_name)()    \
+{                                                                                   \
+  [FrameProcessorPluginRegistry addFrameProcessorPlugin:@"__" @ #name callback:^id(CMSampleBufferRef buffer) {    \
+    return [QRCodeFrameProcessorPlugin callback:buffer];                            \
+  }];                                                                               \
+}
 
 #endif /* FrameProcessorPlugin_h */
