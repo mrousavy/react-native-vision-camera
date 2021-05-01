0553c6b3c0747bf587abca194f4114e556d2afcc
Marc Rousavy <marcrousavy@hotmail.com>
2021-05-01 23:25:55 +0200

    Add docs to create FP Plugins

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   visionSidebar: {
-    Guides: ['guides/setup', 'guides/devices', 'guides/formats', 'guides/capturing', 'guides/frame-processors', 'guides/animated', 'guides/errors', 'guides/troubleshooting'],
+    Guides: ['guides/setup', 'guides/devices', 'guides/formats', 'guides/capturing', 'guides/frame-processors', 'guides/frame-processors-create', 'guides/animated', 'guides/errors', 'guides/troubleshooting'],
     API: require('./typedoc-sidebar.js'),
   },
 };
