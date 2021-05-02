62102a3f8c37f7b3352bdcfdb4213416c4b245f4
Marc Rousavy <marcrousavy@hotmail.com>
2021-05-02 23:10:07 +0200

    docs

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -5,6 +5,7 @@ module.exports = {
         type: 'category',
         label: 'Creating Frame Processor Plugins',
         items: [
+          'guides/frame-processors-plugins-overview',
           'guides/frame-processors-plugins-ios',
           'guides/frame-processors-plugins-android',
           'guides/frame-processors-plugins-final'
