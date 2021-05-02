4fc8cd73ace477a4f8e26e6b1def9d16c246ad8f
Marc Rousavy <marcrousavy@hotmail.com>
2021-05-02 23:20:36 +0200

    community plugins

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -8,7 +8,8 @@ module.exports = {
           'guides/frame-processors-plugins-overview',
           'guides/frame-processors-plugins-ios',
           'guides/frame-processors-plugins-android',
-          'guides/frame-processors-plugins-final'
+          'guides/frame-processors-plugins-final',
+          'guides/frame-processor-plugin-list'
         ]
       }, 'guides/animated', 'guides/errors', 'guides/troubleshooting'],
     API: require('./typedoc-sidebar.js'),
