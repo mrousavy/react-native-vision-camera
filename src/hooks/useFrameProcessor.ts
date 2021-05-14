9c234e61366d961960b6b54e9fcdc2a54fa99b7c
Marc Rousavy <marcrousavy@hotmail.com>
2021-05-14 11:29:59 +0200

    Simplify frame processor

--- src/hooks/useFrameProcessor.ts
+++ src/hooks/useFrameProcessor.ts
@@ -18,10 +18,7 @@ import type { Frame } from 'src/Frame';
  * }, [])
  * ```
  */
-export function useFrameProcessor(frameProcessor: (frame: Frame) => void, dependencies: DependencyList): (frame: Frame) => void {
-  return useCallback((frame: Frame) => {
-    'worklet';
-    return frameProcessor(frame);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, dependencies);
-}
+export const useFrameProcessor: (
+  frameProcessor: (frame: Frame) => void,
+  dependencies: DependencyList,
+) => (frame: Frame) => void = useCallback;
