7935fe9c88769476ae698b29e659350a1c854438
Marc Rousavy <marcrousavy@hotmail.com>
2021-05-14 11:41:47 +0200

    Remove snapshot

--- example/src/Constants.ts
+++ example/src/Constants.ts
@@ -15,12 +15,6 @@ export const SAFE_AREA_PADDING = {
   paddingBottom: SAFE_BOTTOM + CONTENT_SPACING,
 };
 
-// whether to use takeSnapshot() instead of takePhoto() on Android
-export const USE_SNAPSHOT_ON_ANDROID = false;
-
-// whether to use ultra-wide-angle cameras if available, or explicitly disable them. I think ultra-wide-angle cams don't support 60FPS...
-export const USE_ULTRAWIDE_IF_AVAILABLE = true;
-
 // The maximum zoom _factor_ you should be able to zoom in
 export const MAX_ZOOM_FACTOR = 20;
 
