deb8beb688be8245c6aa1e50747462db7a321436
Marc Rousavy <marcrousavy@hotmail.com>
2021-05-27 11:08:57 +0200

    chore: Bump reanimated requirements (#149)
    * Bump the reanimated requirement to 2.2.0 and above
    
    * bump versions podfile
    
    * Simplify Frame Processor
    
    * Add `'worklet'` note

--- ios/Frame Processor/FrameProcessorRuntimeManager.mm
+++ ios/Frame Processor/FrameProcessorRuntimeManager.mm
@@ -29,7 +29,7 @@
       #define ENABLE_FRAME_PROCESSORS
     #endif
   #else
-    #warning Your react-native-reanimated version is not compatible with VisionCamera, Frame Processors are disabled. Make sure you're using reanimated 2.1.0 or above!
+    #warning Your react-native-reanimated version is not compatible with VisionCamera, Frame Processors are disabled. Make sure you're using reanimated 2.2.0 or above!
   #endif
 #else
   #warning The NativeReanimatedModule.h header could not be found, Frame Processors are disabled. If you want to use Frame Processors, make sure you install react-native-reanimated!
@@ -72,16 +72,16 @@ __attribute__((objc_runtime_name("_TtC12VisionCamera10CameraView")))
                                                                   std::make_shared<reanimated::REAIOSErrorHandler>(scheduler),
                                                                   scheduler);
     NSLog(@"FrameProcessorBindings: Runtime Manager created!");
-    
+
     NSLog(@"FrameProcessorBindings: Installing Frame Processor plugins...");
     auto& visionRuntime = *runtimeManager->runtime;
     auto visionGlobal = visionRuntime.global();
     for (NSString* pluginKey in [FrameProcessorPluginRegistry frameProcessorPlugins]) {
       auto pluginName = [pluginKey UTF8String];
-      
+
       NSLog(@"FrameProcessorBindings: Installing Frame Processor plugin \"%s\"...", pluginName);
       FrameProcessorPlugin callback = [[FrameProcessorPluginRegistry frameProcessorPlugins] valueForKey:pluginKey];
-      
+
       auto function = [callback, callInvoker](jsi::Runtime& runtime, const jsi::Value& thisValue, const jsi::Value* arguments, size_t count) -> jsi::Value {
         auto frameHostObject = arguments[0].asObject(runtime).asHostObject(runtime);
         auto frame = static_cast<FrameHostObject*>(frameHostObject.get());
@@ -92,7 +92,7 @@ __attribute__((objc_runtime_name("_TtC12VisionCamera10CameraView")))
         id result = callback(frame->buffer, args);
         return convertObjCObjectToJSIValue(runtime, result);
       };
-      
+
       visionGlobal.setProperty(visionRuntime, pluginName, jsi::Function::createFromHostFunction(visionRuntime,
                                                                                                 jsi::PropNameID::forAscii(visionRuntime, pluginName),
                                                                                                 1, // frame
@@ -120,24 +120,24 @@ __attribute__((objc_runtime_name("_TtC12VisionCamera10CameraView")))
   }
   jsi::Runtime& jsiRuntime = *(jsi::Runtime*)cxxBridge.runtime;
   NSLog(@"FrameProcessorBindings: Installing global functions...");
-  
+
   // setFrameProcessor(viewTag: number, frameProcessor: (frame: Frame) => void)
   auto setFrameProcessor = [self](jsi::Runtime& runtime, const jsi::Value& thisValue, const jsi::Value* arguments, size_t count) -> jsi::Value {
     NSLog(@"FrameProcessorBindings: Setting new frame processor...");
     if (!arguments[0].isNumber()) throw jsi::JSError(runtime, "Camera::setFrameProcessor: First argument ('viewTag') must be a number!");
     if (!arguments[1].isObject()) throw jsi::JSError(runtime, "Camera::setFrameProcessor: Second argument ('frameProcessor') must be a function!");
     if (!runtimeManager || !runtimeManager->runtime) throw jsi::JSError(runtime, "Camera::setFrameProcessor: The RuntimeManager is not yet initialized!");
-    
+
     auto viewTag = arguments[0].asNumber();
     NSLog(@"FrameProcessorBindings: Adapting Shareable value from function (conversion to worklet)...");
     auto worklet = reanimated::ShareableValue::adapt(runtime, arguments[1], runtimeManager.get());
     NSLog(@"FrameProcessorBindings: Successfully created worklet!");
-    
+
     RCTExecuteOnMainQueue([worklet, viewTag, self]() -> void {
       auto currentBridge = [RCTBridge currentBridge];
       auto anonymousView = [currentBridge.uiManager viewForReactTag:[NSNumber numberWithDouble:viewTag]];
       auto view = static_cast<CameraView*>(anonymousView);
-      
+
       dispatch_async(CameraQueues.videoQueue, [worklet, view, self]() -> void {
         NSLog(@"FrameProcessorBindings: Converting worklet to Objective-C callback...");
         auto& rt = *runtimeManager->runtime;
@@ -146,20 +146,20 @@ __attribute__((objc_runtime_name("_TtC12VisionCamera10CameraView")))
         NSLog(@"FrameProcessorBindings: Frame processor set!");
       });
     });
-    
+
     return jsi::Value::undefined();
   };
   jsiRuntime.global().setProperty(jsiRuntime, "setFrameProcessor", jsi::Function::createFromHostFunction(jsiRuntime,
                                                                                                          jsi::PropNameID::forAscii(jsiRuntime, "setFrameProcessor"),
                                                                                                          2,  // viewTag, frameProcessor
                                                                                                          setFrameProcessor));
-  
+
   // unsetFrameProcessor(viewTag: number)
   auto unsetFrameProcessor = [](jsi::Runtime& runtime, const jsi::Value& thisValue, const jsi::Value* arguments, size_t count) -> jsi::Value {
     NSLog(@"FrameProcessorBindings: Removing frame processor...");
     if (!arguments[0].isNumber()) throw jsi::JSError(runtime, "Camera::unsetFrameProcessor: First argument ('viewTag') must be a number!");
     auto viewTag = arguments[0].asNumber();
-    
+
     RCTExecuteOnMainQueue(^{
       auto currentBridge = [RCTBridge currentBridge];
       if (!currentBridge) return;
@@ -169,7 +169,7 @@ __attribute__((objc_runtime_name("_TtC12VisionCamera10CameraView")))
       view.frameProcessorCallback = nil;
       NSLog(@"FrameProcessorBindings: Frame processor removed!");
     });
-    
+
     return jsi::Value::undefined();
   };
   jsiRuntime.global().setProperty(jsiRuntime, "unsetFrameProcessor", jsi::Function::createFromHostFunction(jsiRuntime,
--- src/hooks/useFrameProcessor.ts
+++ src/hooks/useFrameProcessor.ts
@@ -1,10 +1,12 @@
 import { DependencyList, useCallback } from 'react';
 import type { Frame } from 'src/Frame';
 
+type FrameProcessor = (frame: Frame) => void;
+
 /**
  * Returns a memoized Frame Processor function wich you can pass to the `<Camera>`. (See ["Frame Processors"](https://cuvent.github.io/react-native-vision-camera/docs/guides/frame-processors))
  *
- * > If you are using the [react-hooks ESLint plugin](https://www.npmjs.com/package/eslint-plugin-react-hooks), make sure to add `useFrameProcessor` to `additionalHooks` inside your ESLint config. (See ["advanced configuration"](https://www.npmjs.com/package/eslint-plugin-react-hooks#advanced-configuration))
+ * Make sure to add the `'worklet'` directive to the top of the Frame Processor function, otherwise it will not get compiled into a worklet.
  *
  * @param frameProcessor The Frame Processor
  * @param dependencies The React dependencies which will be copied into the VisionCamera JS-Runtime.
@@ -18,10 +20,4 @@ import type { Frame } from 'src/Frame';
  * }, [])
  * ```
  */
-export function useFrameProcessor(frameProcessor: (frame: Frame) => void, dependencies: DependencyList): (frame: Frame) => void {
-  return useCallback((frame: Frame) => {
-    'worklet';
-    return frameProcessor(frame);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, dependencies);
-}
+export const useFrameProcessor: (frameProcessor: FrameProcessor, dependencies: DependencyList) => FrameProcessor = useCallback;
