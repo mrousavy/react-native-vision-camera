2425668d1f890c32898c8f0bd665d0af77bd4414
Marc Rousavy <marcrousavy@hotmail.com>
2021-06-11 15:36:33 +0200

    Create android gradle build setup

---
+++ android/src/main/cpp/VisionCamera.cpp
@@ -0,0 +1,23 @@
+#include <jni.h>
+// #include <jsi/jsi.h>
+// #include <memory>
+// #include <fbjni/fbjni.h>
+// #include <ReactCommon/CallInvokerHolder.h>
+// #include <react/jni/JavaScriptExecutorHolder.h>
+// #include <android/log.h>
+
+// #include "VisionCameraInstaller.h"
+
+// #include "Scheduler.h"
+// #include "AndroidErrorHandler.h"
+// #include "AndroidScheduler.h"
+
+// using namespace facebook;
+// using namespace reanimated;
+
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *) {
+  return facebook::jni::initialize(vm, [] {
+    // TODO: Register natives (hybrid classes)
+    // VisionCameraInstaller::registerNatives();
+  });
+}
