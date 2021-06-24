3ededca2bd4428c79a3472a7fa99a3dc12039853
Marc Rousavy <marcrousavy@hotmail.com>
2021-06-24 11:31:29 +0200

    Update ExamplePluginSwift.swift

--- example/ios/Frame Processor Plugins/Example Plugin (Swift)/ExamplePluginSwift.swift
+++ example/ios/Frame Processor Plugins/Example Plugin (Swift)/ExamplePluginSwift.swift
@@ -13,24 +13,24 @@ import Vision
 public class ExamplePluginSwift: NSObject, FrameProcessorPluginBase {
     @objc
     public static func callback(_ frame: Frame!, withArgs args: [Any]!) -> Any! {
-      guard let imageBuffer = CMSampleBufferGetImageBuffer(frame.buffer) else {
-        return nil
-      }
-      NSLog("ExamplePlugin: \(CVPixelBufferGetWidth(imageBuffer)) x \(CVPixelBufferGetHeight(imageBuffer)) Image. Logging \(args.count) parameters:")
-      
-      args.forEach { arg in
-        NSLog("ExamplePlugin:   -> \(arg) (\(type(of: arg)))")
-      }
-      
-      return [
-        "example_str": "Test",
-        "example_bool": true,
-        "example_double": 5.3,
-        "example_array": [
-          "Hello",
-          true,
-          17.38
+        guard let imageBuffer = CMSampleBufferGetImageBuffer(frame.buffer) else {
+            return nil
+        }
+        NSLog("ExamplePlugin: \(CVPixelBufferGetWidth(imageBuffer)) x \(CVPixelBufferGetHeight(imageBuffer)) Image. Logging \(args.count) parameters:")
+
+        args.forEach { arg in
+            NSLog("ExamplePlugin:   -> \(arg) (\(type(of: arg)))")
+        }
+
+        return [
+            "example_str": "Test",
+            "example_bool": true,
+            "example_double": 5.3,
+            "example_array": [
+                "Hello",
+                true,
+                17.38,
+            ],
         ]
-      ]
     }
 }
