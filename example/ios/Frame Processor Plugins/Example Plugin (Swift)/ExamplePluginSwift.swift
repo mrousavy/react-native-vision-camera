9c71935af89d887ff86f22554957ce7cbf3f9fc7
Marc Rousavy <marcrousavy@hotmail.com>
2021-06-24 11:31:08 +0200

    Rename Swift plugin QR -> Example

---
+++ example/ios/Frame Processor Plugins/Example Plugin (Swift)/ExamplePluginSwift.swift
@@ -0,0 +1,36 @@
+//
+//  ExamplePluginSwift.swift
+//  VisionCamera
+//
+//  Created by Marc Rousavy on 30.04.21.
+//  Copyright © 2021 mrousavy. All rights reserved.
+//
+
+import AVKit
+import Vision
+
+@objc(ExamplePluginSwift)
+public class ExamplePluginSwift: NSObject, FrameProcessorPluginBase {
+    @objc
+    public static func callback(_ frame: Frame!, withArgs args: [Any]!) -> Any! {
+      guard let imageBuffer = CMSampleBufferGetImageBuffer(frame.buffer) else {
+        return nil
+      }
+      NSLog("ExamplePlugin: \(CVPixelBufferGetWidth(imageBuffer)) x \(CVPixelBufferGetHeight(imageBuffer)) Image. Logging \(args.count) parameters:")
+      
+      args.forEach { arg in
+        NSLog("ExamplePlugin:   -> \(arg) (\(type(of: arg)))")
+      }
+      
+      return [
+        "example_str": "Test",
+        "example_bool": true,
+        "example_double": 5.3,
+        "example_array": [
+          "Hello",
+          true,
+          17.38
+        ]
+      ]
+    }
+}
