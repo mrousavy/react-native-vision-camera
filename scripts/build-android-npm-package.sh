a72bfb8cfeef5b1ceca354d3bc4dc876d67da377
Marc Rousavy <marcrousavy@hotmail.com>
2021-06-25 12:09:16 +0200

    Create .aar

---
+++ scripts/build-android-npm-package.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+set -x
+
+ROOT=$(pwd)
+
+rm -rf android-npm/react-native-vision-camera-jsc.aar
+rm -rf android-npm/react-native-vision-camera-hermes.aar
+
+for for_hermes in "True" "False"
+do
+  engine="jsc"
+  if [ "$for_hermes" == "True" ]; then
+    engine="hermes"
+  fi
+  echo "Building VisionCamera for JS Engine ${engine}..."
+
+  cd android
+  ./gradlew clean
+
+  FOR_HERMES=${for_hermes} ./gradlew assembleDebug
+  cd ..
+
+  cp android/build/outputs/aar/*.aar "android-npm/react-native-vision-camera-${engine}.aar"
+  echo "Built react-native-vision-camera-${engine}.aar!"
+done
+
+echo "Finished building VisionCamera packages!"
