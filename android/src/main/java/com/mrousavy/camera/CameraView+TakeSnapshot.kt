da7cbff272c042434122c397081fce91c079fdce
Marc Rousavy <marcrousavy@hotmail.com>
2021-06-25 11:04:17 +0200

    Support `flash` on `takeSnapshot()`

--- android/src/main/java/com/mrousavy/camera/CameraView+TakeSnapshot.kt
+++ android/src/main/java/com/mrousavy/camera/CameraView+TakeSnapshot.kt
@@ -1,6 +1,7 @@
 package com.mrousavy.camera
 
 import android.graphics.Bitmap
+import androidx.camera.core.TorchState
 import androidx.exifinterface.media.ExifInterface
 import com.facebook.react.bridge.Arguments
 import com.facebook.react.bridge.ReadableMap
@@ -11,32 +12,46 @@ import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.withContext
 import java.io.File
 import java.io.FileOutputStream
+import kotlinx.coroutines.guava.await
 
 suspend fun CameraView.takeSnapshot(options: ReadableMap): WritableMap = coroutineScope {
-  val bitmap = this@takeSnapshot.previewView.bitmap ?: throw CameraNotReadyError()
-
-  val quality = if (options.hasKey("quality")) options.getInt("quality") else 100
-
-  val file: File
-  val exif: ExifInterface
-  @Suppress("BlockingMethodInNonBlockingContext")
-  withContext(Dispatchers.IO) {
-    file = File.createTempFile("mrousavy", ".jpg", context.cacheDir).apply { deleteOnExit() }
-    FileOutputStream(file).use { stream ->
-      bitmap.compress(Bitmap.CompressFormat.JPEG, quality, stream)
+  try {
+    val camera = camera ?: throw CameraNotReadyError()
+    val enableFlash = options.getString("flash") == "on"
+    if (enableFlash) {
+      camera.cameraControl.enableTorch(true).await()
     }
-    exif = ExifInterface(file)
+
+    val bitmap = this@takeSnapshot.previewView.bitmap ?: throw CameraNotReadyError()
+
+    val quality = if (options.hasKey("quality")) options.getInt("quality") else 100
+
+    val file: File
+    val exif: ExifInterface
+    @Suppress("BlockingMethodInNonBlockingContext")
+    withContext(Dispatchers.IO) {
+      file = File.createTempFile("mrousavy", ".jpg", context.cacheDir).apply { deleteOnExit() }
+      FileOutputStream(file).use { stream ->
+        bitmap.compress(Bitmap.CompressFormat.JPEG, quality, stream)
+      }
+      exif = ExifInterface(file)
+    }
+
+    val map = Arguments.createMap()
+    map.putString("path", file.absolutePath)
+    map.putInt("width", bitmap.width)
+    map.putInt("height", bitmap.height)
+    map.putBoolean("isRawPhoto", false)
+
+    val skipMetadata =
+      if (options.hasKey("skipMetadata")) options.getBoolean("skipMetadata") else false
+    val metadata = if (skipMetadata) null else exif.buildMetadataMap()
+    map.putMap("metadata", metadata)
+
+    return@coroutineScope map
+  } finally {
+    if (camera?.cameraInfo?.torchState?.value == TorchState.ON) {
+      camera?.cameraControl?.enableTorch(false)
+    }
   }
-
-  val map = Arguments.createMap()
-  map.putString("path", file.absolutePath)
-  map.putInt("width", bitmap.width)
-  map.putInt("height", bitmap.height)
-  map.putBoolean("isRawPhoto", false)
-
-  val skipMetadata = if (options.hasKey("skipMetadata")) options.getBoolean("skipMetadata") else false
-  val metadata = if (skipMetadata) null else exif.buildMetadataMap()
-  map.putMap("metadata", metadata)
-
-  return@coroutineScope map
 }
