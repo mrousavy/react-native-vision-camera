9dad027b4127c2a4460bfdd66d743cf284d0f76e
Marc Rousavy <marcrousavy@hotmail.com>
2021-06-28 17:27:24 +0200

    fix: Fix autolinking for android (#231)
    * Create `react-native.config.js`
    
    * Update react-native.config.js
    
    * Create dummy CameraPackage as proxy for CLI

---
+++ android-npm/src/main/java/com/mrousavy/camera/CameraProxyPackage.java
@@ -0,0 +1,8 @@
+package com.mrousavy.camera;
+
+import com.facebook.react.ReactPackage;
+import com.mrousavy.camera.CameraPackage;
+
+public class CameraPackage implements ReactPackage {
+  // dummy package
+}
