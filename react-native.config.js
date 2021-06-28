992c3b7eba062365359d86fc3d573cd736263195
Marc Rousavy <marcrousavy@hotmail.com>
2021-06-28 17:21:50 +0200

    Update react-native.config.js

--- react-native.config.js
+++ react-native.config.js
@@ -2,7 +2,7 @@ module.exports = {
   dependency: {
     platforms: {
       android: {
-        sourceDir: "android",
+        sourceDir: "./android",
         packageImportPath: "import com.mrousavy.camera.CameraPackage;",
         packageInstance: "new CameraPackage()"
       }
