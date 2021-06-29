35e66b24cc13e692cb17319ff45d6e4cfa2a79fc
Marc Rousavy <marcrousavy@hotmail.com>
2021-06-29 09:17:45 +0200

    Build Android .aar for all major React Native versions

--- scripts/build-android-npm-package.sh
+++ scripts/build-android-npm-package.sh
@@ -5,22 +5,33 @@ ROOT=$(pwd)
 
 rm -rf android-npm/*.aar
 
-for for_hermes in "True" "False"
+
+versions=("0.64.1" "0.63.3")
+versions_majors=("64" "63")
+total_versions=${#versions[@]}
+
+for (( i=0; i<$len; i++ ));
 do
-  engine="jsc"
-  if [ "$for_hermes" == "True" ]; then
-    engine="hermes"
-  fi
-  echo "Building VisionCamera for JS Engine ${engine}..."
 
-  cd android
-  ./gradlew clean
+  yarn add "react-native@${versions[$i]}" -D
+  for for_hermes in "True" "False"
+  do
+    engine="jsc"
+    if [ "$for_hermes" == "True" ]; then
+      engine="hermes"
+    fi
+    echo "Building VisionCamera for React Native ${versions[$i]} (JS Engine ${engine})..."
 
-  FOR_HERMES=${for_hermes} ./gradlew assembleDebug
-  cd ..
+    cd android
+    ./gradlew clean
 
-  cp android/build/outputs/aar/*.aar "android-npm/react-native-vision-camera-${engine}.aar"
-  echo "Built react-native-vision-camera-${engine}.aar!"
+    FOR_HERMES=${for_hermes} ./gradlew assembleDebug
+    cd ..
+
+    cp android/build/outputs/aar/*.aar "android-npm/react-native-vision-camera-${versions_majors[$i]}-${engine}.aar"
+    echo "Built react-native-vision-camera-${versions_majors[$i]}-${engine}.aar!"
+  done
+
 done
 
 echo "Finished building VisionCamera packages!"
