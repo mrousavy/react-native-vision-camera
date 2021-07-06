20268aa7f94cca9cae566e7c10355f128579cc7d
Marc Rousavy <marcrousavy@hotmail.com>
2021-07-06 08:55:40 +0200

    Build Android as source

--- android-npm/src/main/java/com/mrousavy/camera/CameraProxyPackage.java
+++
@@ -1,8 +0,0 @@
-package com.mrousavy.camera;
-
-import com.facebook.react.ReactPackage;
-import com.mrousavy.camera.CameraPackage;
-
-public class CameraPackage implements ReactPackage {
-  // dummy package
-}
