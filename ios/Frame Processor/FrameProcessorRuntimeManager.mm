83ab6810b512b870c0ac12f3f832ba9494765804
Marc Rousavy <marcrousavy@hotmail.com>
2021-07-08 12:18:47 +0200

    fix: Fix UI Thread race condition in `setFrameProcessor(...)`

--- ios/Frame Processor/FrameProcessorRuntimeManager.mm
+++ ios/Frame Processor/FrameProcessorRuntimeManager.mm
@@ -148,20 +148,19 @@ __attribute__((objc_runtime_name("_TtC12VisionCamera10CameraView")))
     auto worklet = reanimated::ShareableValue::adapt(runtime, arguments[1], runtimeManager.get());
     NSLog(@"FrameProcessorBindings: Successfully created worklet!");
 
-    RCTExecuteOnMainQueue([worklet, viewTag, self]() {
-      auto currentBridge = [RCTBridge currentBridge];
-      auto anonymousView = [currentBridge.uiManager viewForReactTag:[NSNumber numberWithDouble:viewTag]];
+    [RCTBridge.currentBridge.uiManager addUIBlock:[worklet, viewTag, self](RCTUIManager*, NSDictionary<NSNumber*, UIView*>* viewRegistry) {
+      auto anonymousView = [viewRegistry objectForKey:[NSNumber numberWithDouble:viewTag]];
       auto view = static_cast<CameraView*>(anonymousView);
-
+      
       dispatch_async(CameraQueues.frameProcessorQueue, [worklet, view, self]() {
         NSLog(@"FrameProcessorBindings: Converting worklet to Objective-C callback...");
         auto& rt = *runtimeManager->runtime;
         auto function = worklet->getValue(rt).asObject(rt).asFunction(rt);
-
+        
         view.frameProcessorCallback = convertJSIFunctionToFrameProcessorCallback(rt, function);
         NSLog(@"FrameProcessorBindings: Frame processor set!");
       });
-    });
+    }];
 
     return jsi::Value::undefined();
   };
