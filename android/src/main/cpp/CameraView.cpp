81936d0184dfda8626b74c8324766d180d523cc7
Marc Rousavy <marcrousavy@hotmail.com>
2021-07-08 13:24:07 +0200

    fix log

--- android/src/main/cpp/CameraView.cpp
+++ android/src/main/cpp/CameraView.cpp
@@ -30,8 +30,7 @@ void CameraView::registerNatives() {
 
 void CameraView::frameProcessorCallback(const alias_ref<JImageProxy::javaobject>& frame) {
   if (frameProcessor_ == nullptr) {
-    __android_log_write(ANDROID_LOG_WARN, TAG, "Frame Processor is null!");
-    setEnableFrameProcessor(false);
+    __android_log_write(ANDROID_LOG_WARN, TAG, "Called Frame Processor callback, but `frameProcessor` is null!");
     return;
   }
 
