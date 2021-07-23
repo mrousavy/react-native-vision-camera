fa6e3d2aeac5bd3056fe9c7b5d9f3bd007f8c1bd
Marc Rousavy <marcrousavy@hotmail.com>
2021-07-23 17:34:19 +0200

    Create custom `VisionCameraScheduler`

---
+++ ios/Frame Processor/VisionCameraScheduler.mm
@@ -0,0 +1,47 @@
+//
+//  VisionCameraScheduler.mm
+//  VisionCamera
+//
+//  Created by Marc Rousavy on 23.07.21.
+//  Copyright © 2021 mrousavy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "VisionCameraScheduler.h"
+
+#import <React-callinvoker/ReactCommon/CallInvoker.h>
+
+// Forward declarations for the Swift classes
+__attribute__((objc_runtime_name("_TtC12VisionCamera12CameraQueues")))
+@interface CameraQueues : NSObject
+@property (nonatomic, class, readonly, strong) dispatch_queue_t _Nonnull frameProcessorQueue;
+@end
+
+namespace vision {
+  
+using namespace facebook;
+  
+VisionCameraScheduler::VisionCameraScheduler(std::shared_ptr<react::CallInvoker> jsInvoker) {
+  this->jsCallInvoker_ = jsInvoker;
+}
+
+// does not schedule on UI thread but rather on Frame Processor Thread
+void VisionCameraScheduler::scheduleOnUI(std::function<void()> job) {
+  if (runtimeManager.lock() == nullptr) {
+    return;
+  }
+  
+  __block std::weak_ptr<reanimated::RuntimeManager> blockRuntimeManager = runtimeManager;
+  
+  dispatch_async(CameraQueues.frameProcessorQueue, ^{
+    if (blockRuntimeManager.lock()) {
+      job();
+    }
+  });
+}
+
+VisionCameraScheduler::~VisionCameraScheduler(){
+}
+  
+  
+} // namespace vision
