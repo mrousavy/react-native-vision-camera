d1dd675db8c154f53a5e1d5754b510d4df23bb56
Marc Rousavy <marcrousavy@hotmail.com>
2021-10-05 12:01:24 +0200

    Fix RNGH for Android

--- example/android/app/src/main/java/com/mrousavy/camera/example/MainActivity.java
+++ example/android/app/src/main/java/com/mrousavy/camera/example/MainActivity.java
@@ -1,6 +1,9 @@
 package com.mrousavy.camera.example;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
@@ -17,5 +20,15 @@ public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(null);
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 }
