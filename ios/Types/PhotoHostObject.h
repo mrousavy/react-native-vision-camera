25b155c41d4f393b339b49f730f6a9f26a1b3a40
Marc Rousavy <me@mrousavy.com>
2022-01-04 09:01:29 +0100

    Create `PhotoHostObject`

---
+++ ios/Types/PhotoHostObject.h
@@ -0,0 +1,11 @@
+//
+//  PhotoHostObject.h
+//  VisionCamera
+//
+//  Created by Marc Rousavy on 04.01.22.
+//  Copyright © 2022 mrousavy. All rights reserved.
+//
+
+#pragma once
+
+#import <AVFoundation.h>
