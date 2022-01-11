3907093031a66a4bc3d46d97fdece61166fd4b31
Hirbod Mirjavadi <hm@nightstomp.com>
2022-01-11 12:10:17 +0100

    refactor: no need to pass a param here. We just want to disbale it

--- src/expo-plugin/withDisableFrameProcessorsAndroid.ts
+++ src/expo-plugin/withDisableFrameProcessorsAndroid.ts
@@ -4,7 +4,7 @@ import { ConfigPlugin, withGradleProperties } from '@expo/config-plugins';
  * Set the `disableFrameProcessors` value in the static `gradle.properties` file.
  * This is used to disable frame processors if you don't need it for android.
  */
-export const withDisableFrameProcessorsAndroid: ConfigPlugin<boolean> = (c, isDisabled) => {
+export const withDisableFrameProcessorsAndroid: ConfigPlugin = (c) => {
   const disableFrameProcessorsKey = 'disableFrameProcessors';
   return withGradleProperties(c, (config) => {
     config.modResults = config.modResults.filter((item) => {
@@ -15,7 +15,7 @@ export const withDisableFrameProcessorsAndroid: ConfigPlugin<boolean> = (c, isDi
     config.modResults.push({
       type: 'property',
       key: disableFrameProcessorsKey,
-      value: String(isDisabled),
+      value: 'true',
     });
 
     return config;
