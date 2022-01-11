362932e7c00529fa35573dadc49f036f2a5de841
Nils Krause <NilsKrause@users.noreply.github.com>
2022-01-11 20:16:10 +0100

    Use ViewGroupManager instead of SimpleViewManager

--- android/src/main/java/com/mrousavy/camera/CameraViewManager.kt
+++ android/src/main/java/com/mrousavy/camera/CameraViewManager.kt
@@ -3,12 +3,12 @@ package com.mrousavy.camera
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReadableMap
 import com.facebook.react.common.MapBuilder
-import com.facebook.react.uimanager.SimpleViewManager
+import com.facebook.react.uimanager.ViewGroupManager
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.annotations.ReactProp
 
 @Suppress("unused")
-class CameraViewManager(reactContext: ReactApplicationContext) : SimpleViewManager<CameraView>() {
+class CameraViewManager(reactContext: ReactApplicationContext) : ViewGroupManager<CameraView>() {
 
   public override fun createViewInstance(context: ThemedReactContext): CameraView {
     val cameraViewModule = context.getNativeModule(CameraViewModule::class.java)!!
