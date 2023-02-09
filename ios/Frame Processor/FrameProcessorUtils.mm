a3de924d4b0a21160effd926f914c3dfa54c5046
Marc Rousavy <me@mrousavy.com>
2023-02-09 15:46:19 +0100

    Use RN Worklets 0.2.5

--- ios/Frame Processor/FrameProcessorUtils.mm
+++ ios/Frame Processor/FrameProcessorUtils.mm
@@ -22,6 +22,9 @@
 #import "JsiWorklet.h"
 
 FrameProcessorCallback convertWorkletToFrameProcessorCallback(jsi::Runtime& runtime, std::shared_ptr<RNWorklet::JsiWorklet> worklet) {
+  
+  auto workletInvoker = std::make_shared<RNWorklet::WorkletInvoker>(worklet);
+  
   // Converts a Worklet to a callable Objective-C block function
   return ^(Frame* frame) {
 
@@ -30,7 +33,7 @@ FrameProcessorCallback convertWorkletToFrameProcessorCallback(jsi::Runtime& runt
       // Call JS Frame Processor function with boxed Frame Host Object
       auto argument = jsi::Object::createFromHostObject(runtime, frameHostObject);
       jsi::Value jsValue(std::move(argument));
-      worklet->call(runtime, jsi::Value::undefined(), &jsValue, 1);
+      workletInvoker->call(runtime, jsi::Value::undefined(), &jsValue, 1);
     } catch (jsi::JSError& jsError) {
       auto stack = std::regex_replace(jsError.getStack(), std::regex("\n"), "\n    ");
       auto message = [NSString stringWithFormat:@"Frame Processor threw an error: %s\nIn: %s", jsError.getMessage().c_str(), stack.c_str()];
