6ea4cdfbb776b1e6993fb6645bc5140c81f0ece8
Marc Rousavy <me@mrousavy.com>
2023-02-09 10:04:53 +0100

    chore: Upgrade Example to RN 0.71

--- example/android/app/src/main/java/com/mrousavy/camera/example/MainActivity.java
+++ example/android/app/src/main/java/com/mrousavy/camera/example/MainActivity.java
@@ -2,8 +2,8 @@ package com.mrousavy.camera.example;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
 import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
@@ -17,18 +17,26 @@ public class MainActivity extends ReactActivity {
     return "VisionCameraExample";
   }
 
+  /**
+   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
+   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+   * (aka React 18) with two boolean flags.
+   */
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new DefaultReactActivityDelegate(
+        this,
+        getMainComponentName(),
+        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+        DefaultNewArchitectureEntryPoint.getFabricEnabled(), // fabricEnabled
+        // If you opted-in for the New Architecture, we enable Concurrent React (i.e. React 18).
+        DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
+      );
   }
 
   @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegate(this, getMainComponentName()) {
-      @Override
-      protected ReactRootView createRootView() {
-        return new RNGestureHandlerEnabledRootView(MainActivity.this);
-      }
-    };
+  protected void onCreate(Bundle savedInstanceState) {
+    // RN Screens workaround
+    super.onCreate(null);
   }
 }
