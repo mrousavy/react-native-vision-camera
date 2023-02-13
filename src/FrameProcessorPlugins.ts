d7c3351fca791020dcaa6d7033f12e2c13b1097d
Marc Rousavy <me@mrousavy.com>
2023-02-13 15:21:04 +0100

    fix: Simplify FP Plugins Proxy

--- src/FrameProcessorPlugins.ts
+++ src/FrameProcessorPlugins.ts
@@ -7,28 +7,7 @@ Camera.installFrameProcessorBindings();
 type FrameProcessor = (frame: Frame, ...args: unknown[]) => unknown;
 type TFrameProcessorPlugins = Record<string, FrameProcessor>;
 
-function getPluginsProxy(): TFrameProcessorPlugins {
-  if (__DEV__) {
-    // Proxy to catch dev errors
-    return new Proxy({} as TFrameProcessorPlugins, {
-      get: (_, name) => {
-        const pluginsProxy = global.FrameProcessorPlugins;
-
-        if (pluginsProxy == null) throw new Error('Frame Processor Plugins Proxy is null! Are Frame Processors initialized properly?');
-
-        if (typeof name === 'symbol') return pluginsProxy[name];
-
-        return pluginsProxy[name];
-
-        if (plugin == null)
-          throw new Error(`The Frame Processor Plugin "${name}" does not exist! Are you sure you exposed it properly in native code?`);
-
-        return plugin;
-      },
-    });
-  }
-
-  return global.FrameProcessorPlugins as TFrameProcessorPlugins;
-}
-
-export const FrameProcessorPlugins = getPluginsProxy();
+/**
+ * All natively installed Frame Processor Plugins.
+ */
+export const FrameProcessorPlugins = global.FrameProcessorPlugins as TFrameProcessorPlugins;
