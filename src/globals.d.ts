4b9baa37d3dcc82cfcd958d5263239dbc8dcb237
Marc Rousavy <me@mrousavy.com>
2023-02-15 19:15:36 +0100

    fix: Fix `global.FrameProcessorPlugins` TS error

--- src/globals.d.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-var */
-
-/**
- * The global Frame Processor plugins registry - will be initialized after the `installFrameProcessorBindings()` call
- */
-declare var FrameProcessorPlugins: Record<string | symbol, unknown> | undefined;
