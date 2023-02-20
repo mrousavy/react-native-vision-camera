f6e82c1ffbf146aa7429145c4f2f28d84106c657
Marc Rousavy <me@mrousavy.com>
2023-02-20 17:52:40 +0100

    Check Format before converting

--- android/src/main/java/com/mrousavy/camera/utils/ImageProxy+toBitmap.kt
+++ android/src/main/java/com/mrousavy/camera/utils/ImageProxy+toBitmap.kt
@@ -5,20 +5,25 @@ import androidx.camera.core.ImageProxy
 import java.io.ByteArrayOutputStream
 
 fun ImageProxy.toBitmap(): Bitmap {
-  val yBuffer = planes[0].buffer // Y
-  val vuBuffer = planes[2].buffer // VU
+  when (format) {
+    ImageFormat.YUV_420_888 -> {
+      val yBuffer = planes[0].buffer // Y
+      val vuBuffer = planes[2].buffer // VU
 
-  val ySize = yBuffer.remaining()
-  val vuSize = vuBuffer.remaining()
+      val ySize = yBuffer.remaining()
+      val vuSize = vuBuffer.remaining()
 
-  val nv21 = ByteArray(ySize + vuSize)
+      val nv21 = ByteArray(ySize + vuSize)
 
-  yBuffer.get(nv21, 0, ySize)
-  vuBuffer.get(nv21, ySize, vuSize)
+      yBuffer.get(nv21, 0, ySize)
+      vuBuffer.get(nv21, ySize, vuSize)
 
-  val yuvImage = YuvImage(nv21, ImageFormat.NV21, this.width, this.height, null)
-  val out = ByteArrayOutputStream()
-  yuvImage.compressToJpeg(Rect(0, 0, yuvImage.width, yuvImage.height), 100, out)
-  val imageBytes = out.toByteArray()
-  return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+      val yuvImage = YuvImage(nv21, ImageFormat.NV21, this.width, this.height, null)
+      val out = ByteArrayOutputStream()
+      yuvImage.compressToJpeg(Rect(0, 0, yuvImage.width, yuvImage.height), 100, out)
+      val imageBytes = out.toByteArray()
+      return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+    }
+    else -> throw Error("Invalid Image Format! Cannot convert ImageProxy with Format \"$format\" to Bitmap.")
+  }
 }
