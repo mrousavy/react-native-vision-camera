284a635334238e914c45e77c37046e1449e28381
Marc Rousavy <me@mrousavy.com>
2023-02-20 13:11:05 +0100

    Update Shaders.ts

--- example/src/Shaders.ts
+++ example/src/Shaders.ts
@@ -29,6 +29,33 @@ half4 main(vec2 pos) {
 }
 `;
 
+export const BLUR_SHADER = `
+const int samples = 35,
+          LOD = 2,         // gaussian done on MIPmap at scale LOD
+          sLOD = 1 << LOD; // tile size = 2^LOD
+const float sigma = float(samples) * .25;
+
+float gaussian(vec2 i) {
+    return exp( -.5* dot(i/=sigma,i) ) / ( 6.28 * sigma*sigma );
+}
+
+vec4 blur(sampler2D sp, vec2 U, vec2 scale) {
+    vec4 O = vec4(0);
+    int s = samples/sLOD;
+
+    for ( int i = 0; i < s*s; i++ ) {
+        vec2 d = vec2(i%s, i/s)*float(sLOD) - float(samples)/2.;
+        O += gaussian(d) * textureLod( sp, U + scale * d , float(LOD) );
+    }
+
+    return O / O.a;
+}
+
+void mainImage(out vec4 O, vec2 U) {
+    O = blur( iChannel0, U/iResolution.xy, 1./iChannelResolution[0].xy );
+}
+`;
+
 export const FACE_SHADER = `
 uniform shader image;
 uniform float x;
