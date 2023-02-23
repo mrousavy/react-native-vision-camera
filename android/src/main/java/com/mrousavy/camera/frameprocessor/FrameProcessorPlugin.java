d9c90d8138c0d6373686c0b874919dc4f44080f3
Marc Rousavy <me@mrousavy.com>
2023-02-23 18:45:05 +0100

    Remove CameraX annotations

--- android/src/main/java/com/mrousavy/camera/frameprocessor/FrameProcessorPlugin.java
+++ android/src/main/java/com/mrousavy/camera/frameprocessor/FrameProcessorPlugin.java
@@ -16,7 +16,7 @@ public abstract class FrameProcessorPlugin {
 
     /**
      * The actual Frame Processor plugin callback. Called for every frame the ImageAnalyzer receives.
-     * @param image The CameraX ImageProxy. Don't call .close() on this, as VisionCamera handles that.
+     * @param image The Frame. Don't call .close() on this, as VisionCamera handles that.
      * @return You can return any primitive, map or array you want. See the
      * <a href="https://mrousavy.github.io/react-native-vision-camera/docs/guides/frame-processors-plugins-overview#types">Types</a>
      * table for a list of supported types.
