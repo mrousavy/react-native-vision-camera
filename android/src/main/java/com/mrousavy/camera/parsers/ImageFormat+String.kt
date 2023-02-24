8a679629998fba1ee8c80514bc92eee31c20fe68
Marc Rousavy <me@mrousavy.com>
2023-02-24 12:31:10 +0100

    Add Android specific formats

--- android/src/main/java/com/mrousavy/camera/parsers/ImageFormat+String.kt
+++ android/src/main/java/com/mrousavy/camera/parsers/ImageFormat+String.kt
@@ -2,22 +2,18 @@ package com.mrousavy.camera.parsers
 
 import android.graphics.ImageFormat
 
-/**
- * Parses ImageFormat/PixelFormat int to a string representation useable for the TypeScript types.
- */
 fun parseImageFormat(imageFormat: Int): String {
   return when (imageFormat) {
-    ImageFormat.YUV_420_888 -> "yuv"
-    ImageFormat.YUV_422_888 -> "yuv"
-    ImageFormat.YUV_444_888 -> "yuv"
+    ImageFormat.YUV_420_888 -> "420v"
+    ImageFormat.YUV_422_888 -> "422v"
+    ImageFormat.YUV_444_888 -> "444v"
     ImageFormat.JPEG -> "jpeg"
-    ImageFormat.DEPTH_JPEG -> "jpeg-depth"
+    ImageFormat.DEPTH_JPEG -> "jpeg"
     ImageFormat.RAW_SENSOR -> "raw"
     ImageFormat.RAW_PRIVATE -> "raw"
     ImageFormat.HEIC -> "heic"
     ImageFormat.PRIVATE -> "private"
-    ImageFormat.DEPTH16 -> "depth-16"
-    else -> "unknown"
+    else -> "private"
     /*
     ImageFormat.UNKNOWN -> "TODOFILL"
     ImageFormat.RGB_565 -> "TODOFILL"
--- src/PixelFormat.ts
+++ src/PixelFormat.ts
@@ -5,5 +5,11 @@
  * * `x420`: 4:2:0 YpCbCr 10-Bit Bi-Planar Video Range (HDR)
  * * `422v`: 4:2:2 YpCbCr 8-Bit Bi-Planar Video Range
  * * `444v`: 4:4:4 YpCbCr 8-Bit Bi-Planar Video Range
+ *
+ * Android specific Formats:
+ * * `private`: Private hardware specific implementation of the pixmap.
+ * * `raw`: RAW hardware specific implementation.
+ * * `jpeg`: JPEG format for Frames.
+ * * `heic`: HEIC brand of High Efficiency Image File Format as described in ISO/IEC 23008-12
  */
-export type PixelFormat = '420v' | '420f' | 'x420' | '422v' | '444v';
+export type PixelFormat = '420v' | '420f' | 'x420' | '422v' | '444v' | 'private' | 'raw' | 'jpeg' | 'heic';
