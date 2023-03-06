77dd181da9d5239df52c72752816d1ed33bf94a1
Marc Rousavy <me@mrousavy.com>
2023-03-06 11:40:46 +0100

    docs: fix meta

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -56,8 +56,8 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title="VisionCamera Documentation"
+      description="📸 The Camera library that sees the vision.">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
