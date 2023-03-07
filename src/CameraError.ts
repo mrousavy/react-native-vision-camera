2fd7fcce3fd5719e87ecaec6df5bde286ad44446
Marc Rousavy <me@mrousavy.com>
2023-03-07 09:35:25 +0100

    Assert JSI is available

--- src/CameraError.ts
+++ src/CameraError.ts
@@ -50,7 +50,7 @@ export type CaptureError =
   | 'capture/photo-not-enabled'
   | 'capture/aborted'
   | 'capture/unknown';
-export type SystemError = 'system/no-camera-manager' | 'system/view-not-found';
+export type SystemError = 'system/camera-module-not-found' | 'system/no-camera-manager' | 'system/view-not-found';
 export type UnknownError = 'unknown/unknown';
 
 /**
@@ -154,7 +154,7 @@ class CameraError<TCode extends CameraErrorCode> extends Error {
  *
  * See the ["Camera Errors" documentation](https://mrousavy.github.io/react-native-vision-camera/docs/guides/errors) for more information about Camera Errors.
  */
-export class CameraCaptureError extends CameraError<CaptureError> {}
+export class CameraCaptureError extends CameraError<CaptureError> { }
 
 /**
  * Represents any kind of error that occured in the Camera View Module.
@@ -163,7 +163,7 @@ export class CameraCaptureError extends CameraError<CaptureError> {}
  */
 export class CameraRuntimeError extends CameraError<
   PermissionError | ParameterError | DeviceError | FormatError | FrameProcessorError | SessionError | SystemError | UnknownError
-> {}
+> { }
 
 /**
  * Checks if the given `error` is of type {@linkcode ErrorWithCause}
--- src/NativeCameraModule.ts
+++ src/NativeCameraModule.ts
@@ -1,27 +1,25 @@
 import { NativeModules, Platform } from 'react-native';
+import { CameraRuntimeError } from './CameraError';
 
 // NativeModules automatically resolves 'CameraView' to 'CameraViewModule'
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 export const CameraModule = NativeModules.CameraView;
 if (CameraModule == null) {
-  let message =
-    'Failed to initialize VisionCamera: The native Camera Module (`NativeModules.CameraView`) could not be found.';
-  message +=
-    '\n* Make sure react-native-vision-camera is correctly autolinked (run `npx react-native config` to verify)';
-  if (Platform.OS === 'ios' || Platform.OS === 'macos') {
-    message += '\n* Make sure you ran `pod install` in the ios/ directory.';
-  }
-  if (Platform.OS === 'android') {
-    message += '\n* Make sure gradle is synced.';
-  }
+  let message = 'Failed to initialize VisionCamera: The native Camera Module (`NativeModules.CameraView`) could not be found.';
+  message += '\n* Make sure react-native-vision-camera is correctly autolinked (run `npx react-native config` to verify)';
+  if (Platform.OS === 'ios' || Platform.OS === 'macos') message += '\n* Make sure you ran `pod install` in the ios/ directory.';
+
+  if (Platform.OS === 'android') message += '\n* Make sure gradle is synced.';
+
   // check if Expo
-  const ExpoConstants =
-    NativeModules.NativeUnimoduleProxy?.modulesConstants?.ExponentConstants;
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const ExpoConstants = NativeModules.NativeUnimoduleProxy?.modulesConstants?.ExponentConstants;
   if (ExpoConstants != null) {
     if (ExpoConstants.appOwnership === 'expo') {
       // We're running Expo Go
-      throw new Error(
-        'react-native-vision-camera is not supported in Expo Go! Use EAS (`expo prebuild`) or eject to a bare workflow instead.'
+      throw new CameraRuntimeError(
+        'system/camera-module-not-found',
+        'react-native-vision-camera is not supported in Expo Go! Use EAS (`expo prebuild`) or eject to a bare workflow instead.',
       );
     } else {
       // We're running Expo bare / standalone
@@ -30,5 +28,5 @@ if (CameraModule == null) {
   }
 
   message += '\n* Make sure you rebuilt the app.';
-  throw new Error(message);
+  throw new CameraRuntimeError('system/camera-module-not-found', message);
 }
