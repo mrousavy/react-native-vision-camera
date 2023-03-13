a058885720dde5aeba90a1492fbb3c7b1b5d5ca8
Marc Rousavy <me@mrousavy.com>
2023-03-13 09:48:30 -0400

    Update NativeCameraModule.ts

--- src/NativeCameraModule.ts
+++ src/NativeCameraModule.ts
@@ -17,7 +17,6 @@ if (CameraModule == null) {
   let message = 'Failed to initialize VisionCamera: The native Camera Module (`NativeModules.CameraView`) could not be found.';
   message += '\n* Make sure react-native-vision-camera is correctly autolinked (run `npx react-native config` to verify)';
   if (Platform.OS === 'ios' || Platform.OS === 'macos') message += '\n* Make sure you ran `pod install` in the ios/ directory.';
-
   if (Platform.OS === 'android') message += '\n* Make sure gradle is synced.';
 
   // check if Expo
@@ -25,17 +24,16 @@ if (CameraModule == null) {
   const ExpoConstants = NativeModules.NativeUnimoduleProxy?.modulesConstants?.ExponentConstants;
   if (ExpoConstants != null) {
     if (ExpoConstants.appOwnership === 'expo') {
-      // We're running Expo Go
       throw new CameraRuntimeError(
         'system/camera-module-not-found',
-        `react-native-vision-camera is not supported in Expo Go! Use EAS/expo prebuild instead (\`expo run:${Platform.OS}\`). For more info, see https://docs.expo.dev/workflow/prebuild/.`,
+        'react-native-vision-camera is not supported in Expo Go! Use EAS/expo prebuild instead (`npx expo prebuild`). For more info, see https://docs.expo.dev/workflow/prebuild/',
       );
     } else {
-      // We're running Expo bare / standalone
-      message += '\n* Make sure you ran `expo prebuild`.';
+      message += `\n* Make sure you rebuilt the app using \`npx expo prebuild\` and \`npx expo run:${Platform.OS}\`. For more info, see https://docs.expo.dev/workflow/prebuild/`;
     }
   }
 
   message += '\n* Make sure you rebuilt the app.';
+
   throw new CameraRuntimeError('system/camera-module-not-found', message);
 }
