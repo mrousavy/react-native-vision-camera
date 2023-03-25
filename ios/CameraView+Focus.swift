71433317652be92653d9fe657222f7b6368673dc
Levi <levi@yourwebfile.com>
2023-03-24 23:42:30 -0700

    fix ios focus

--- ios/CameraView+Focus.swift
+++ ios/CameraView+Focus.swift
@@ -80,18 +80,52 @@ extension CameraView {
         try device.lockForConfiguration()
 
         device.focusPointOfInterest = normalizedPoint
-        device.focusMode = .continuousAutoFocus
+        device.focusMode = .autoFocus
 
         if device.isExposurePointOfInterestSupported {
           device.exposurePointOfInterest = normalizedPoint
-          device.exposureMode = .continuousAutoExposure
+          device.exposureMode = .autoExpose
         }
+
+        // Enable subject area change monitoring
+        device.isSubjectAreaChangeMonitoringEnabled = true
+
+        // Remove any existing observer for subject area change notifications
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.AVCaptureDeviceSubjectAreaDidChange, object: nil)
+
+        // Register observer for subject area change notifications
+        NotificationCenter.default.addObserver(self, selector: #selector(subjectAreaDidChange), name: NSNotification.Name.AVCaptureDeviceSubjectAreaDidChange, object: nil)
 
         device.unlockForConfiguration()
         return nil
       } catch {
         throw CameraError.device(DeviceError.configureError)
       }
+    }
+  }
+
+  @objc func subjectAreaDidChange(notification: NSNotification) {
+    guard let device = self.videoDeviceInput?.device else {
+      invokeOnError(.session(.cameraNotReady))
+      return
+    }
+    do {
+      try device.lockForConfiguration()
+
+      // Reset focus and exposure settings to continuous mode
+      if device.isFocusPointOfInterestSupported {
+        device.focusMode = .continuousAutoFocus
+      }
+
+      if device.isExposurePointOfInterestSupported {
+        device.exposureMode = .continuousAutoExposure
+      }
+      
+      device.isSubjectAreaChangeMonitoringEnabled = false
+
+      device.unlockForConfiguration()
+    } catch {
+      invokeOnError(.device(.configureError))
     }
   }
 }
