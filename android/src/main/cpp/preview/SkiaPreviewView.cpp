1c8ec0abba05d93c6568b9484571f314e6b2df55
Marc Rousavy <me@mrousavy.com>
2023-05-05 20:06:21 +0200

    Try to draw

--- android/src/main/cpp/preview/SkiaPreviewView.cpp
+++ android/src/main/cpp/preview/SkiaPreviewView.cpp
@@ -6,6 +6,10 @@
 
 #include <android/log.h>
 
+#include "SkImage.h"
+#include "SkSurface.h"
+
+
 namespace vision {
 
 using TSelf = jni::local_ref<SkiaPreviewView::jhybriddata>;
@@ -32,13 +36,9 @@ void SkiaPreviewView::onSurfaceTextureAvailable(const facebook::jni::alias_ref<j
                                                 jint width, jint height) {
     __android_log_write(ANDROID_LOG_INFO, TAG,
                         "onSurfaceTextureAvailable!");
+    _width = width;
+    _height = height;
     _skiaRenderer = std::make_unique<RNSkia::SkiaOpenGLRenderer>(surface.get());
-}
-
-void SkiaPreviewView::onSurfaceTextureSizeChanged(const facebook::jni::alias_ref<jobject>& surface,
-                                                jint width, jint height) {
-    __android_log_write(ANDROID_LOG_INFO, TAG,
-                        "onSurfaceTextureSizeChanged!");
 }
 
 void SkiaPreviewView::onSurfaceTextureDestroyed(const facebook::jni::alias_ref<jobject>& surface) {
@@ -46,13 +46,31 @@ void SkiaPreviewView::onSurfaceTextureDestroyed(const facebook::jni::alias_ref<j
                         "onSurfaceTextureDestroyed!");
     if (_skiaRenderer != nullptr) {
         _skiaRenderer->teardown();
+        _skiaRenderer = nullptr;
     }
+}
+
+void SkiaPreviewView::onSurfaceTextureSizeChanged(const facebook::jni::alias_ref<jobject>& surface,
+                                                  jint width, jint height) {
+    __android_log_write(ANDROID_LOG_INFO, TAG,
+                        "onSurfaceTextureSizeChanged!");
+    _width = width;
+    _height = height;
 }
 
 void SkiaPreviewView::onSurfaceTextureUpdated(const facebook::jni::alias_ref<jobject>& surface) {
     __android_log_write(ANDROID_LOG_INFO, TAG,
                         "onSurfaceTextureUpdated!");
-
+    if (_skiaRenderer != nullptr) {
+        _skiaRenderer->run([](SkCanvas* canvas) {
+            __android_log_write(ANDROID_LOG_INFO, TAG,
+                                "draw callback executing...");
+            SkRect rect = SkRect::MakeXYWH(300, 300, 300, 400);
+            SkPaint paint(SkColors::kRed);
+            canvas->drawRect(rect, paint);
+            canvas->flush();
+        }, _width, _height);
+    }
 }
 
 
