4b7fa853faab161661fa264ecbb3452e811e3cf7
Marc Rousavy <me@mrousavy.com>
2023-06-22 15:40:08 +0200

    tflite

---
+++ example/ios/Frame Processor Plugins/TensorflowPlugin/TensorflowFrameProcessorPlugin.mm
@@ -0,0 +1,46 @@
+//
+//  TensorflowFrameProcessorPlugin.m
+//  VisionCameraExample
+//
+//  Created by Marc Rousavy on 22.06.23.
+//
+
+#import <Foundation/Foundation.h>
+#import <VisionCamera/FrameProcessorPlugin.h>
+#import <VisionCamera/Frame.h>
+#import <TensorFlowLite/TensorFlowLite.h>
+
+@interface TensorflowFrameProcessorPlugin : FrameProcessorPlugin
+@end
+
+@implementation TensorflowFrameProcessorPlugin
+
+- (NSString *)name {
+  return @"tensorflow";
+}
+
+- (id)callback:(Frame *)frame withArguments:(NSArray<id> *)arguments {
+  CVPixelBufferRef imageBuffer = CMSampleBufferGetImageBuffer(frame.buffer);
+  NSLog(@"ExamplePlugin: %zu x %zu Image. Logging %lu parameters:", CVPixelBufferGetWidth(imageBuffer), CVPixelBufferGetHeight(imageBuffer), (unsigned long)arguments.count);
+  
+  for (id param in arguments) {
+    NSLog(@"ExamplePlugin:   -> %@ (%@)", param == nil ? @"(nil)" : [param description], NSStringFromClass([param classForCoder]));
+  }
+  
+  return @{
+    @"example_str": @"Test",
+    @"example_bool": @true,
+    @"example_double": @5.3,
+    @"example_array": @[
+      @"Hello",
+      @true,
+      @17.38
+    ]
+  };
+}
+
++ (void) load {
+  [self registerPlugin:[[TensorflowFrameProcessorPlugin alloc] init]];
+}
+
+@end
