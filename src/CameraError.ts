9fcf5dbed06bb4abb3069eebb0d867773d44fc6e
Marc Rousavy <me@mrousavy.com>
2023-07-07 15:29:17 +0200

    Rename error to `system/frame-processors-unavailable`

--- src/CameraError.ts
+++ src/CameraError.ts
@@ -15,7 +15,6 @@ export type DeviceError =
   | 'device/low-light-boost-not-supported'
   | 'device/focus-not-supported'
   | 'device/camera-not-available-on-simulator';
-export type FrameProcessorError = 'frame-processor/unavailable';
 export type FormatError =
   | 'format/invalid-fps'
   | 'format/invalid-hdr'
@@ -110,7 +109,6 @@ type CameraErrorCode =
   | PermissionError
   | ParameterError
   | DeviceError
-  | FrameProcessorError
   | FormatError
   | SessionError
   | CaptureError
@@ -167,7 +165,7 @@ export class CameraCaptureError extends CameraError<CaptureError> {}
  * See the ["Camera Errors" documentation](https://mrousavy.github.io/react-native-vision-camera/docs/guides/errors) for more information about Camera Errors.
  */
 export class CameraRuntimeError extends CameraError<
-  PermissionError | ParameterError | DeviceError | FormatError | FrameProcessorError | SessionError | SystemError | UnknownError
+  PermissionError | ParameterError | DeviceError | FormatError | SessionError | SystemError | UnknownError
 > {}
 
 /**
