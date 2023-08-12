01af4d8446d4c91f26acc19d3bb700d837bb5836
Marc Rousavy <me@mrousavy.com>
2023-08-12 11:08:21 +0200

    Fix Semaphore 0

--- android/src/main/java/com/mrousavy/camera/extensions/Handler+postAndWait.kt
+++ android/src/main/java/com/mrousavy/camera/extensions/Handler+postAndWait.kt
@@ -7,8 +7,7 @@ import java.util.concurrent.Semaphore
  * Posts a Message to this Handler and blocks the calling Thread until the Handler finished executing the given job.
  */
 fun Handler.postAndWait(job: () -> Unit) {
-  val semaphore = Semaphore(1)
-  semaphore.drainPermits()
+  val semaphore = Semaphore(0)
 
   this.post {
     try {
