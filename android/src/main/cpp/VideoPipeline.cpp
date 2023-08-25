499d9135194b0129e1a1e69c721e3adeb9d2e60a
Marc Rousavy <me@mrousavy.com>
2023-08-25 12:24:45 +0200

    Create `VideoPipeline` c++

---
+++ android/src/main/cpp/VideoPipeline.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Marc Rousavy on 25.08.23.
+//
+
+#include "VideoPipeline.h"
+
+namespace vision {
+
+// TODO: Implement pipeline
+
+} // vision
---
+++ android/src/main/cpp/VideoPipeline.h
@@ -0,0 +1,17 @@
+//
+// Created by Marc Rousavy on 25.08.23.
+//
+
+#pragma once
+
+#include <jni.h>
+#include <fbjni/fbjni.h>
+
+namespace vision {
+
+class VideoPipeline {
+
+};
+
+} // vision
+
