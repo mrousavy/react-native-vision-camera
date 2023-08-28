6e38032f5f2a5aad94cbd77a0b3c5abf2c388a4a
Marc Rousavy <me@mrousavy.com>
2023-08-28 11:16:15 +0200

    Create `PassThroughShader`

---
+++ android/src/main/cpp/PassThroughShader.cpp
@@ -0,0 +1,74 @@
+//
+// Created by Marc Rousavy on 28.08.23.
+//
+
+#include "PassThroughShader.h"
+#include <EGL/egl.h>
+#include <memory>
+#include "OpenGLError.h"
+#include <string>
+
+namespace vision {
+
+
+PassThroughShader::PassThroughShader() {
+  _programId = createProgram();
+}
+
+PassThroughShader::~PassThroughShader() {
+  glDeleteProgram(_programId);
+}
+
+GLuint PassThroughShader::getProgramId() const {
+  return _programId;
+}
+
+const GLfloat* PassThroughShader::getVertexData() const {
+  return VERTEX_DATA;
+}
+
+const GLushort* PassThroughShader::getVertexIndices() const {
+  return VERTEX_INDICES;
+}
+
+GLuint PassThroughShader::loadShader(GLenum shaderType, const char* shaderCode) {
+  GLuint shader = glCreateShader(shaderType);
+  if (shader == 0) throw OpenGLError("Failed to load shader!");
+
+  glShaderSource(shader, 1, &shaderCode, nullptr);
+  glCompileShader(shader);
+  GLint compileStatus = GL_FALSE;
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &compileStatus);
+  if (compileStatus == GL_FALSE) {
+    glDeleteShader(shader);
+    throw OpenGLError("Failed to compile shader!");
+  }
+  return shader;
+}
+
+GLuint PassThroughShader::createProgram() {
+  GLuint vertexShader = loadShader(GL_VERTEX_SHADER, VERTEX_SHADER);
+  GLuint fragmentShader = loadShader(GL_FRAGMENT_SHADER, FRAGMENT_SHADER);
+
+  GLuint program = glCreateProgram();
+  if (program == 0) throw OpenGLError("Failed to create pass-through program!");
+
+  glAttachShader(program, vertexShader);
+  if (glGetError() != GL_NO_ERROR) throw OpenGLError("Failed to attach Vertex Shader!");
+
+  glAttachShader(program, fragmentShader);
+  if (glGetError() != GL_NO_ERROR) throw OpenGLError("Failed to attach Fragment Shader!");
+
+  glLinkProgram(program);
+  GLint linkStatus = GL_FALSE;
+  glGetProgramiv(program, GL_LINK_STATUS, &linkStatus);
+  if (!linkStatus) {
+    glDeleteProgram(program);
+    throw OpenGLError("Failed to load pass-through program!");
+  }
+  return program;
+}
+
+
+
+} // vision
---
+++ android/src/main/cpp/PassThroughShader.h
@@ -0,0 +1,61 @@
+//
+// Created by Marc Rousavy on 28.08.23.
+//
+
+#pragma once
+
+#include <EGL/egl.h>
+#include <GLES2/gl2.h>
+
+namespace vision {
+
+class PassThroughShader {
+
+ public:
+  explicit PassThroughShader();
+  ~PassThroughShader();
+
+  GLuint getProgramId() const;
+
+  const GLfloat* getVertexData() const;
+  const GLushort* getVertexIndices() const;
+
+ private:
+  GLuint _programId;
+  GLuint loadShader(GLenum shaderType, const char* shaderCode);
+  GLuint createProgram();
+
+ private:
+  static constexpr GLfloat VERTEX_DATA[] = {
+      -1.0f, -1.0f, 0.0, 1.0, 0.0f, 0.0f,
+      +1.0f, -1.0f, 0.0, 1.0, 1.0f, 0.0f,
+      -1.0f, +1.0f, 0.0, 1.0, 0.0f, 1.0f,
+      +1.0f, +1.0f, 0.0, 1.0, 1.0f, 1.0f,
+  };
+  static constexpr GLushort VERTEX_INDICES[] = {
+      0, 1, 2, 3
+  };
+
+  static constexpr char VERTEX_SHADER[] =
+      "uniform mat4 uTransformMatrix;\n"
+      "uniform mat4 uRotationMatrix;\n"
+      "attribute vec4 aPosition;\n"
+      "attribute vec4 aTexCoord;\n"
+      "varying vec2 vTexCoord;\n"
+      "void main() {\n"
+      "    gl_Position = uRotationMatrix * aPosition;\n"
+      "    vTexCoord = (uTransformMatrix * aTexCoord).xy;\n"
+      "}\n";
+  static constexpr char FRAGMENT_SHADER[] =
+      "#extension GL_OES_EGL_image_external:require\n"
+      "precision mediump float;\n"
+      "uniform samplerExternalOES uTexture;\n"
+      "varying vec2 vTexCoord;\n"
+      "void main() {\n"
+      "    gl_FragColor = texture2D(uTexture, vTexCoord);\n"
+      "}\n";
+
+};
+
+} // vision
+
