033ddb8cff384cd26af0215f87bbc916e3c59dbc
Marc Rousavy <me@mrousavy.com>
2023-09-01 19:39:14 +0200

    Move prettier and babel to separate files

---
+++ package/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "bracketSpacing": true,
+  "bracketSameLine": true,
+  "singleQuote": true,
+  "trailingComma": "all",
+  "semi": true,
+  "tabWidth": 2,
+  "useTabs": false,
+  "printWidth": 140
+}
