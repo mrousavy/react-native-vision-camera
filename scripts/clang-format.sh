b2ea240bbb576b19f75fb4baac60c323340bdc29
Marc Rousavy <me@mrousavy.com>
2023-09-01 16:21:00 +0200

    Update clang-format.sh

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -2,7 +2,6 @@
 
 if which clang-format >/dev/null; then
   find cpp ios android/src/main/cpp -type f \( -name "*.h" -o -name "*.cpp" -o -name "*.m" -o -name "*.mm" \) -print0 | while read -d $'\0' file; do
-    echo "-> cpp-lint $file"
     clang-format -i "$file"
   done
 else
