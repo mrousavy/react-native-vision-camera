a1ccd697b9f2f73f9173732bff0fe306d2a33a77
Marc Rousavy <me@mrousavy.com>
2023-09-01 12:43:01 +0200

    Update clang-format.sh

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 if which clang-format >/dev/null; then
-  for file in $(find cpp ios android/src/main/cpp -type f \( -name "*.h" -o -name "*.cpp" \)); do
-      clang-format -i "$file"
+  find cpp ios android/src/main/cpp -type f \( -name "*.h" -o -name "*.cpp" -o -name "*.m" -o -name "*.mm" \) -print0 | while read -d $'\0' file; do
+    echo "-> cpp-lint $file"
+    clang-format -i "$file"
   done
+  # for file in $(find cpp ios android/src/main/cpp -type f \( -name "*.h" -o -name "*.cpp" \)); do
+  #     echo "$file"
+  #     clang-format -i "$file"
+  # done
 else
   echo "warning: clang-format not installed, download from https://clang.llvm.org/docs/ClangFormat.html (or run brew install clang-format)"
 fi
