548045101bce026826e262c1bbd9aac6d2553145
Marc Rousavy <me@mrousavy.com>
2023-09-01 17:04:53 +0200

    Move `.clang-format` to `cpp/`

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -2,7 +2,7 @@
 
 if which clang-format >/dev/null; then
   find cpp ios android/src/main/cpp -type f \( -name "*.h" -o -name "*.cpp" -o -name "*.m" -o -name "*.mm" \) -print0 | while read -d $'\0' file; do
-    clang-format -i "$file"
+    clang-format -style=file:./cpp/.clang-format -i "$file"
   done
 else
   echo "warning: clang-format not installed, download from https://clang.llvm.org/docs/ClangFormat.html (or run brew install clang-format)"
