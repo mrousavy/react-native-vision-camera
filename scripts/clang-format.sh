9177e60ace73c1bd895c586440aedd484ea2dfab
Marc Rousavy <me@mrousavy.com>
2023-09-01 12:37:32 +0200

    Only search in cpp dirs

--- scripts/clang-format.sh
+++ scripts/clang-format.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if which clang-format >/dev/null; then
-  for file in $(find . -type f \( -name "*.h" -o -name "*.cpp" \)); do
+  for file in $(find cpp ios android/src/main/cpp -type f \( -name "*.h" -o -name "*.cpp" \)); do
       clang-format -i "$file"
   done
 else
