47fbe37e33688c27a1f8f7ce169eb786c864d243
Marc Rousavy <me@mrousavy.com>
2023-09-04 14:39:29 +0200

    docs: Restructure FP docs

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -6,18 +6,25 @@ module.exports = {
       'guides/lifecycle',
       'guides/formats',
       'guides/capturing',
-      'guides/frame-processors',
       {
         type: 'category',
-        label: 'Creating Frame Processor Plugins',
+        label: 'Realtime Frame Processing',
         items: [
-          'guides/frame-processors-plugins-overview',
-          'guides/frame-processors-plugins-ios',
-          'guides/frame-processors-plugins-android',
-          'guides/frame-processors-plugins-final',
+          'guides/frame-processors',
+          'guides/frame-processor-plugin-list',
+          'guides/skia-frame-processors',
+          {
+            type: 'category',
+            label: 'Creating Frame Processor Plugins',
+            items: [
+              'guides/frame-processors-plugins-overview',
+              'guides/frame-processors-plugins-ios',
+              'guides/frame-processors-plugins-android',
+              'guides/frame-processors-plugins-final',
+            ]
+          },
         ]
       },
-      'guides/frame-processor-plugin-list',
       'guides/zooming',
       'guides/focusing',
       'guides/errors',
