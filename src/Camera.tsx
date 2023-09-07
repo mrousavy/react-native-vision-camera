401ea32fca7e1dd2b959abf80ad79b6a4622ba5f
Marc Rousavy <me@mrousavy.com>
2023-09-07 12:12:18 +0200

    fix: Optionally require Reanimated

--- src/Camera.tsx
+++ src/Camera.tsx
@@ -10,8 +10,7 @@ import type { PhotoFile, TakePhotoOptions } from './PhotoFile';
 import type { Point } from './Point';
 import type { TakeSnapshotOptions } from './Snapshot';
 import type { CameraVideoCodec, RecordVideoOptions, VideoFile } from './VideoFile';
-import { createWorkletRuntime } from 'react-native-reanimated';
-import { makeShareableCloneRecursive } from 'react-native-reanimated/src/reanimated2/shareables';
+import { FrameProcessorContext } from './FrameProcessorContext';
 
 //#region Types
 export type CameraPermissionStatus = 'authorized' | 'not-determined' | 'denied' | 'restricted';
@@ -438,7 +437,7 @@ export class Camera extends React.PureComponent<CameraProps> {
   private setFrameProcessor(frameProcessor: (frame: Frame) => void): void {
     this.assertFrameProcessorsEnabled();
     // @ts-expect-error JSI functions aren't typed
-    global.setFrameProcessor(this.handle, makeShareableCloneRecursive(frameProcessor), createWorkletRuntime('VisionCamera'));
+    global.setFrameProcessor(this.handle, FrameProcessorContext.createWorklet(frameProcessor), FrameProcessorContext.workletRuntime);
   }
 
   private unsetFrameProcessor(): void {
