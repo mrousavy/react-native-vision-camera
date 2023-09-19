4ceb5313464455522b4381acee414c5b1bf77e96
Marc Rousavy <me@mrousavy.com>
2023-09-19 09:13:24 +0200

    fix: Consider everything between `24mm` and `43mm` a wide-angle camera lens
    cc @AndrewGable

--- android/src/main/java/com/mrousavy/camera/utils/CameraCharacteristicsUtils.kt
+++ android/src/main/java/com/mrousavy/camera/utils/CameraCharacteristicsUtils.kt
@@ -26,7 +26,6 @@ val Size35mm = Size(36, 24)
  * * [Ultra-Wide-Angle Lens (wikipedia)](https://en.wikipedia.org/wiki/Ultra_wide_angle_lens)
  */
 fun CameraCharacteristics.getDeviceTypes(): ReadableArray {
-  // TODO: Check if getDeviceType() works correctly, even for logical multi-cameras
   val focalLengths = this.get(CameraCharacteristics.LENS_INFO_AVAILABLE_FOCAL_LENGTHS)!!
   val sensorSize = this.get(CameraCharacteristics.SENSOR_INFO_PHYSICAL_SIZE)!!
 
@@ -35,17 +34,19 @@ fun CameraCharacteristics.getDeviceTypes(): ReadableArray {
 
   val deviceTypes = Arguments.createArray()
 
-  val containsTelephoto = focalLengths.any { l -> (l * cropFactor) > 35 } // TODO: Telephoto lenses are > 85mm, but we don't have anything between that range..
-  // val containsNormalLens = focalLengths.any { l -> (l * cropFactor) > 35 && (l * cropFactor) <= 55 }
-  val containsWideAngle = focalLengths.any { l -> (l * cropFactor) >= 24 && (l * cropFactor) <= 35 }
-  val containsUltraWideAngle = focalLengths.any { l -> (l * cropFactor) < 24 }
-
-  if (containsTelephoto)
-    deviceTypes.pushString("telephoto-camera")
-  if (containsWideAngle)
-    deviceTypes.pushString("wide-angle-camera")
-  if (containsUltraWideAngle)
-    deviceTypes.pushString("ultra-wide-angle-camera")
+  focalLengths.forEach { focalLength ->
+    // scale to the 35mm film standard
+    val l = focalLength * cropFactor
+    when {
+      // https://en.wikipedia.org/wiki/Ultra_wide_angle_lens
+      l < 24f -> deviceTypes.pushString("ultra-wide-angle-camera")
+      // https://en.wikipedia.org/wiki/Wide-angle_lens
+      l in 24f..43f -> deviceTypes.pushString("wide-angle-camera")
+      // https://en.wikipedia.org/wiki/Telephoto_lens
+      l > 43f -> deviceTypes.pushString("telephoto-camera")
+      else -> throw Error("Invalid focal length! (${focalLength}mm)")
+    }
+  }
 
   return deviceTypes
 }
