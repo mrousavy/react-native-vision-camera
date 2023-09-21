27be2986e6fc97d1602313264060e30aab7b895e
Marc Rousavy <me@mrousavy.com>
2023-09-21 11:33:06 +0200

    Fix exports

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,16 +1,21 @@
 export * from './Camera';
 export * from './CameraDevice';
 export * from './CameraError';
+export * from './CameraProps';
 export { Frame } from './Frame';
 export * from './FrameProcessorPlugins';
-export * from './CameraProps';
+export * from './Orientation';
 export * from './PhotoFile';
 export * from './PixelFormat';
 export * from './Point';
 export * from './VideoFile';
 
-export * from './hooks/useCameraDevices';
-export * from './hooks/useCameraDevice';
-export * from './hooks/useCameraFormat';
+export * from './devices/Filter';
 export * from './devices/getCameraFormat';
+export * from './devices/getCameraDevice';
+
+export * from './hooks/useCameraDevice';
+export * from './hooks/useCameraDevices';
+export * from './hooks/useCameraFormat';
+export * from './hooks/useCameraPermission';
 export * from './hooks/useFrameProcessor';
