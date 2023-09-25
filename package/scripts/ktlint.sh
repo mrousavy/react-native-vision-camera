979656d37915dfe92f211b04d94327416cea1caa
Marc Rousavy <me@mrousavy.com>
2023-09-25 13:04:22 +0200

    chore: Update install links

--- package/scripts/ktlint.sh
+++ package/scripts/ktlint.sh
@@ -3,5 +3,5 @@
 if which ktlint >/dev/null; then
   cd android && ktlint --color --relative --editorconfig=./.editorconfig -F ./**/*.kt*
 else
-  echo "warning: KTLint not installed, download from https://github.com/pinterest/ktlint"
+  echo "warning: KTLint not installed, install with 'brew install ktlint' (or manually from https://github.com/pinterest/ktlint)"
 fi
