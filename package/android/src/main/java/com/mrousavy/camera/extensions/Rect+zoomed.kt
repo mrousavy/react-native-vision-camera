207833608ae81e380010c0eb260d5330c6109853
touchmarine <scout@touchlabs.io>
2023-09-27 23:08:16 +0200

    fix: Incorrect zoom on Android < 11
    Fixes #1865

--- package/android/src/main/java/com/mrousavy/camera/extensions/Rect+zoomed.kt
+++ package/android/src/main/java/com/mrousavy/camera/extensions/Rect+zoomed.kt
@@ -3,12 +3,11 @@ package com.mrousavy.camera.extensions
 import android.graphics.Rect
 
 fun Rect.zoomed(zoomFactor: Float): Rect {
-  val height = bottom - top
-  val width = right - left
-
-  val left = this.left + (width / zoomFactor / 2)
-  val top = this.top + (height / zoomFactor / 2)
-  val right = this.right - (width / zoomFactor / 2)
-  val bottom = this.bottom - (height / zoomFactor / 2)
-  return Rect(left.toInt(), top.toInt(), right.toInt(), bottom.toInt())
+  val dx = (width() / zoomFactor / 2).toInt()
+  val dy = (height() / zoomFactor / 2).toInt()
+  val left = centerX() - dx
+  val top = centerY() - dy
+  val right = centerX() + dx
+  val bottom = centerY() + dy
+  return Rect(left, top, right, bottom)
 }
