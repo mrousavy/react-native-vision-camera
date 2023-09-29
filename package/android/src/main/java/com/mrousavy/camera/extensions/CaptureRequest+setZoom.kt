2885d2f966bc226af6da8491c4bc718b5bc7159f
Marc Rousavy <marcrousavy@hotmail.com>
2023-09-29 19:41:31 +0200

    Update package/android/src/main/java/com/mrousavy/camera/extensions/CaptureRequest+setZoom.kt

--- package/android/src/main/java/com/mrousavy/camera/extensions/CaptureRequest+setZoom.kt
+++ package/android/src/main/java/com/mrousavy/camera/extensions/CaptureRequest+setZoom.kt
@@ -6,18 +6,15 @@ import android.os.Build
 import android.util.Range
 
 fun CaptureRequest.Builder.setZoom(zoom: Float, cameraCharacteristics: CameraCharacteristics) {
-    val zoomRange = (
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-        cameraCharacteristics.get(CameraCharacteristics.CONTROL_ZOOM_RATIO_RANGE)
-      } else {
-        null
-      }
-      ) ?: Range(1f, cameraCharacteristics.get(CameraCharacteristics.SCALER_AVAILABLE_MAX_DIGITAL_ZOOM) ?: 1f)
-    val zoomClamped = zoomRange.clamp(zoom)
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+      val zoomRange = cameraCharacteristics.get(CameraCharacteristics.CONTROL_ZOOM_RATIO_RANGE) ?: Range(1f, 1f)
+      val zoomClamped = zoomRange.clamp(zoom)
       this.set(CaptureRequest.CONTROL_ZOOM_RATIO, zoomClamped)
     } else {
+      val maxZoom = cameraCharacteristics.get(CameraCharacteristics.SCALER_AVAILABLE_MAX_DIGITAL_ZOOM)
+      val zoomRange = Range(1f, maxZoom ?: 1f)
       val size = cameraCharacteristics.get(CameraCharacteristics.SENSOR_INFO_ACTIVE_ARRAY_SIZE)!!
+      val zoomClamped = zoomRange.clamp(zoom)
       this.set(CaptureRequest.SCALER_CROP_REGION, size.zoomed(zoomClamped))
     }
 }
