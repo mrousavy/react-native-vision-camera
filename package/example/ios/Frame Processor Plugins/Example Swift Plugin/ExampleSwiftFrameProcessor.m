2375622b0732c64755b35aaef34b4bf6aa261819
Mateusz Mędrek <mateusz.medrek@callstack.com>
2023-10-02 19:25:27 +0200

    chore(docs): add Swift FP plugin

---
+++ package/example/ios/Frame Processor Plugins/Example Swift Plugin/ExampleSwiftFrameProcessor.m
@@ -0,0 +1,31 @@
+//
+//  ExampleSwiftFrameProcessor.m
+//  VisionCameraExample
+//
+//  Created by Mateusz Medrek on 02/10/2023.
+//
+
+#if __has_include(<VisionCamera/FrameProcessorPlugin.h>)
+#import <Foundation/Foundation.h>
+#import <VisionCamera/FrameProcessorPlugin.h>
+#import <VisionCamera/FrameProcessorPluginRegistry.h>
+#import <VisionCamera/Frame.h>
+
+#import "VisionCameraExample-Swift.h"
+
+// Example for a Swift Frame Processor plugin automatic registration
+@interface ExampleSwiftFrameProcessorPlugin (FrameProcessorPluginLoader)
+@end
+
+@implementation ExampleSwiftFrameProcessorPlugin (FrameProcessorPluginLoader)
+
++ (void)load
+{
+  [FrameProcessorPluginRegistry addFrameProcessorPlugin:@"example_kotlin_swift_plugin" withInitializer:^FrameProcessorPlugin * _Nonnull(NSDictionary * _Nullable options) {
+    return [[ExampleSwiftFrameProcessorPlugin alloc] init];
+  }];
+}
+
+@end
+
+#endif
