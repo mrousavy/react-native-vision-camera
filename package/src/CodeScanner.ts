c5de69a93b74c8c001a3d97e91aa6bb08a2af0f6
Marc Rousavy <me@mrousavy.com>
2023-10-03 23:27:45 +0200

    docs: Add comments

--- package/src/CodeScanner.ts
+++ package/src/CodeScanner.ts
@@ -19,8 +19,17 @@ export type CodeType =
  * A scanned code.
  */
 export interface Code {
+  /**
+   * The type of the code that was scanned.
+   */
   type: CodeType | 'unknown'
+  /**
+   * The string value, or null if it cannot be decoded.
+   */
   value?: string
+  /**
+   * The location of the code relative to the Camera Preview (in dp).
+   */
   frame?: {
     x: number
     y: number
@@ -33,8 +42,17 @@ export interface Code {
  * A scanner for detecting codes in a Camera Stream.
  */
 export interface CodeScanner {
+  /**
+   * The types of codes to configure the code scanner for.
+   */
   codeTypes: CodeType[]
+  /**
+   * A callback to call whenever the scanned codes change.
+   */
   onCodeScanned: (codes: Code[]) => void
+  /**
+   * Crops the scanner's view area to the specific region of interest.
+   */
   regionOfInterest?: {
     x: number
     y: number
