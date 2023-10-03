223aaa1c0dba4adddbc869a6f989713ce69b4a09
Marc Rousavy <me@mrousavy.com>
2023-10-03 23:25:29 +0200

    fix: iOS use Preview coordinate system

--- package/ios/CameraView+CodeScanner.swift
+++ package/ios/CameraView+CodeScanner.swift
@@ -10,7 +10,7 @@ import AVFoundation
 import Foundation
 
 extension CameraView: AVCaptureMetadataOutputObjectsDelegate {
-  public func metadataOutput(_: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from _: AVCaptureConnection) {
+  public func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from _: AVCaptureConnection) {
     guard let onCodeScanned = onCodeScanned else {
       return
     }
@@ -21,15 +21,16 @@ extension CameraView: AVCaptureMetadataOutputObjectsDelegate {
       if let code = object as? AVMetadataMachineReadableCodeObject {
         value = code.stringValue
       }
+      let frame = previewView.layerRectConverted(fromMetadataOutputRect: object.bounds)
 
       return [
         "type": object.type.descriptor,
         "value": value as Any,
         "frame": [
-          "x": object.bounds.origin.x,
-          "y": object.bounds.origin.y,
-          "width": object.bounds.size.width,
-          "height": object.bounds.size.height,
+          "x": frame.origin.x,
+          "y": frame.origin.y,
+          "width": frame.size.width,
+          "height": frame.size.height,
         ],
       ]
     }
--- package/ios/PreviewView.swift
+++ package/ios/PreviewView.swift
@@ -37,6 +37,10 @@ class PreviewView: UIView {
   override public class var layerClass: AnyClass {
     return AVCaptureVideoPreviewLayer.self
   }
+  
+  func layerRectConverted(fromMetadataOutputRect rect: CGRect) -> CGRect {
+    return videoPreviewLayer.layerRectConverted(fromMetadataOutputRect: rect)
+  }
 
   init(frame: CGRect, session: AVCaptureSession) {
     super.init(frame: frame)
