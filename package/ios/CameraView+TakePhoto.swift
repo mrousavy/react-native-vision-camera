49a05faf0af2f9cdccfffc8edc05e36bada5204c
Marc Rousavy <me@mrousavy.com>
2023-10-05 17:02:55 +0200

    feat: Set `PhotoOutput`'s maximum photo resolution

--- package/ios/CameraView+TakePhoto.swift
+++ package/ios/CameraView+TakePhoto.swift
@@ -62,6 +62,11 @@ extension CameraView {
         }
         photoSettings.photoQualityPrioritization = photoQualityPrioritization
       }
+      
+      // photo size is always the one selected in the format
+      if #available(iOS 16.0, *) {
+        photoSettings.maxPhotoDimensions = photoOutput.maxPhotoDimensions
+      }
 
       // red-eye reduction
       if #available(iOS 12.0, *), let autoRedEyeReduction = options["enableAutoRedEyeReduction"] as? Bool {
