a498144fd5786c6dba1d78730d362e0f675cbaf7
Marc Rousavy <me@mrousavy.com>
2023-10-09 11:17:07 +0200

    chore: Update doc URLs

--- package/src/expo-plugin/withVisionCamera.ts
+++ package/src/expo-plugin/withVisionCamera.ts
@@ -27,7 +27,7 @@ type Props = {
   /**
    * Whether to enable the Frame Processors runtime, or explicitly disable it.
    * Disabling Frame Processors will make your app smaller as the C++ files will not be compiled.
-   * See [Frame Processors](https://www.react-native-vision-camera.com/docs/guides/frame-processors)
+   * See [Frame Processors](https://react-native-vision-camera.com/docs/guides/frame-processors)
    * @default false
    */
   disableFrameProcessors?: boolean
@@ -35,7 +35,7 @@ type Props = {
    * Whether to enable the QR/Barcode Scanner Model. If true, the MLKit Model will
    * automatically be downloaded on app startup. If false, it will be downloaded
    * once the Camera is created with a `CodeScanner`.
-   * See [QR/Barcode Scanning](https://www.react-native-vision-camera.com/docs/guides/code-scanning)
+   * See [QR/Barcode Scanning](https://react-native-vision-camera.com/docs/guides/code-scanning)
    * @default false
    */
   enableCodeScanner?: boolean
