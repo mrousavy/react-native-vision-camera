b082a1457d181667312e97e950d3e3c3e23b2a1d
Marc Rousavy <me@mrousavy.com>
2023-10-19 11:09:05 +0200

    Update ExampleFrameProcessorPlugin.java

--- package/example/android/app/src/main/java/com/mrousavy/camera/example/ExampleFrameProcessorPlugin.java
+++ package/example/android/app/src/main/java/com/mrousavy/camera/example/ExampleFrameProcessorPlugin.java
@@ -42,6 +42,6 @@ public class ExampleFrameProcessorPlugin extends FrameProcessorPlugin {
 
     ExampleFrameProcessorPlugin(@Nullable Map<String, Object> options) {
         super(options);
-        Log.d("ExamplePlugin", " - options: " + options.toString());
+        Log.d("ExamplePlugin", " - options: " + if (options != null) options.toString() else "(null)");
     }
 }
