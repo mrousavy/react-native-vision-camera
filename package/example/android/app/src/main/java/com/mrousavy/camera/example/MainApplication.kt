72d5e7c8e9080cba1277b1d7c19b71555531c2c5
Marc Rousavy <me@mrousavy.com>
2023-12-07 17:30:05 +0100

    Init CameraPackage on Android

--- package/example/android/app/src/main/java/com/mrousavy/camera/example/MainApplication.kt
+++ package/example/android/app/src/main/java/com/mrousavy/camera/example/MainApplication.kt
@@ -10,6 +10,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
+import com.mrousavy.camera.CameraPackage
 import com.mrousavy.camera.frameprocessor.FrameProcessorPluginRegistry
 
 class MainApplication : Application(), ReactApplication {
@@ -25,8 +26,9 @@ class MainApplication : Application(), ReactApplication {
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> {
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          return PackageList(this).packages
+          val packages = PackageList(this).packages
+          packages.add(CameraPackage())
+          return packages
         }
 
         override fun getJSMainModuleName(): String = "index"
