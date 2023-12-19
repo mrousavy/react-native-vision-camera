302c7fa1a61f6dd92e41b8101544496e2e4a17f5
David Gruseck <david.gruseck@actinate.com>
2023-12-19 15:53:50 +0100

    fix for inset of SENSOR_INFO_ACTIVE_ARRAY_SIZE

--- package/android/src/main/java/com/mrousavy/camera/extensions/Rect+zoomed.kt
+++ package/android/src/main/java/com/mrousavy/camera/extensions/Rect+zoomed.kt
@@ -5,9 +5,9 @@ import android.graphics.Rect
 fun Rect.zoomed(zoomFactor: Float): Rect {
   val dx = (width() / zoomFactor / 2).toInt()
   val dy = (height() / zoomFactor / 2).toInt()
-  val left = centerX() - dx
-  val top = centerY() - dy
-  val right = centerX() + dx
-  val bottom = centerY() + dy
+  val left = centerX() - this.left - dx
+  val top = centerY() - this.top - dy
+  val right = centerX() - this.left + dx
+  val bottom = centerY() - this.top + dy
   return Rect(left, top, right, bottom)
 }
