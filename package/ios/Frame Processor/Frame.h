c71c6af2e27f08a8f4d6b9cb5cd02ac22bc425bf
Marc Rousavy <me@mrousavy.com>
2024-01-16 19:14:40 +0100

    fix: Use property accessors on `Frame`

--- package/ios/Frame Processor/Frame.h
+++ package/ios/Frame Processor/Frame.h
@@ -14,21 +14,21 @@
 
 @interface Frame : NSObject
 
-- (instancetype _Nonnull)initWithBuffer:(CMSampleBufferRef _Nonnull)buffer orientation:(UIImageOrientation)orientation;
+- (instancetype _Nonnull)initWithBuffer:(CMSampleBufferRef _Nonnull)buffer
+                            orientation:(UIImageOrientation)orientation;
 
 - (instancetype)init NS_UNAVAILABLE;
 
-@property(nonatomic, readonly) CMSampleBufferRef _Nonnull buffer;
+@property(nonatomic, readonly, nonnull) CMSampleBufferRef _Nonnull buffer;
 @property(nonatomic, readonly) UIImageOrientation orientation;
 
-// Getters
-- (NSString* _Nonnull)pixelFormat;
-- (BOOL)isMirrored;
-- (BOOL)isValid;
-- (size_t)width;
-- (size_t)height;
-- (double)timestamp;
-- (size_t)bytesPerRow;
-- (size_t)planesCount;
+@property(nonatomic, readonly, nonnull) NSString* pixelFormat;
+@property(nonatomic, readonly) BOOL isMirrored;
+@property(nonatomic, readonly) BOOL isValid;
+@property(nonatomic, readonly) size_t width;
+@property(nonatomic, readonly) size_t height;
+@property(nonatomic, readonly) double timestamp;
+@property(nonatomic, readonly) size_t bytesPerRow;
+@property(nonatomic, readonly) size_t planesCount;
 
 @end
