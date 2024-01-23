0b6cfedf2d5976c2dcfe588c5eac818a3e2ecb7d
Janic Duplessis <janicduplessis@gmail.com>
2024-01-23 17:35:06 -0500

    Update CameraView+Focus.swift

--- ios/CameraView+Focus.swift
+++ ios/CameraView+Focus.swift
@@ -22,20 +22,61 @@ extension CameraView {
 
       do {
         try device.lockForConfiguration()
+        defer {
+          device.unlockForConfiguration()
+        }
 
+        // Set Focus
         device.focusPointOfInterest = normalizedPoint
         device.focusMode = .autoFocus
 
+        // Set Exposure
         if device.isExposurePointOfInterestSupported {
           device.exposurePointOfInterest = normalizedPoint
           device.exposureMode = .autoExpose
         }
+        
+        // Remove any existing listeners
+        NotificationCenter.default.removeObserver(self,
+                                                  name: NSNotification.Name.AVCaptureDeviceSubjectAreaDidChange,
+                                                  object: nil)
 
-        device.unlockForConfiguration()
+        // Listen for focus completion
+        device.isSubjectAreaChangeMonitoringEnabled = true
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(subjectAreaDidChange),
+                                               name: NSNotification.Name.AVCaptureDeviceSubjectAreaDidChange,
+                                               object: nil)
+
         return nil
       } catch {
         throw CameraError.device(DeviceError.configureError)
       }
     }
+  }
+  
+  @objc
+  func subjectAreaDidChange(notification _: NSNotification) {
+    guard let device = videoDeviceInput?.device else {
+      return
+    }
+
+    try? device.lockForConfiguration()
+    defer {
+      device.unlockForConfiguration()
+    }
+
+    // Reset Focus to continuous/auto
+    if device.isFocusPointOfInterestSupported {
+      device.focusMode = .continuousAutoFocus
+    }
+
+    // Reset Exposure to continuous/auto
+    if device.isExposurePointOfInterestSupported {
+      device.exposureMode = .continuousAutoExposure
+    }
+
+    // Disable listeners
+    device.isSubjectAreaChangeMonitoringEnabled = false
   }
 }
