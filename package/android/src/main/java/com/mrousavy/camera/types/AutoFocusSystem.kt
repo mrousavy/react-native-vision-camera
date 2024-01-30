bafeb578daf0e728b752be77147fa591a1ad5543
Marc Rousavy <me@mrousavy.com>
2024-01-30 10:41:39 +0100

    fix: Expose auto-focus system for Android

--- package/android/src/main/java/com/mrousavy/camera/types/AutoFocusSystem.kt
+++ package/android/src/main/java/com/mrousavy/camera/types/AutoFocusSystem.kt
@@ -1,6 +1,7 @@
 package com.mrousavy.camera.types
 
 enum class AutoFocusSystem(override val unionValue: String) : JSUnionValue {
+  PHASE_DETECTION("phase-detection"),
   CONTRAST_DETECTION("contrast-detection"),
   NONE("none");
 
