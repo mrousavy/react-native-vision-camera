5175e6dd80f41d248f13c383ca029a44784d9de3
Marc Rousavy <me@mrousavy.com>
2024-02-01 14:45:34 +0100

    Update PersistentCameraCaptureSession.kt

--- package/android/src/main/java/com/mrousavy/camera/core/PersistentCameraCaptureSession.kt
+++ package/android/src/main/java/com/mrousavy/camera/core/PersistentCameraCaptureSession.kt
@@ -5,23 +5,50 @@ import android.hardware.camera2.CameraDevice
 import android.hardware.camera2.CameraManager
 import android.hardware.camera2.CaptureRequest
 import android.hardware.camera2.TotalCaptureResult
+import android.util.Log
 import com.mrousavy.camera.core.outputs.SurfaceOutput
 import com.mrousavy.camera.extensions.capture
 import com.mrousavy.camera.extensions.createCaptureSession
+import com.mrousavy.camera.extensions.openCamera
 import java.io.Closeable
 
 /**
  * A [CameraCaptureSession] wrapper that safely handles interruptions and remains open whenever available.
  */
-class PersistentCameraCaptureSession(private val cameraManager: CameraManager,
-                                     private val device: CameraDevice): Closeable {
-  private var session: CameraCaptureSession? = null
+class PersistentCameraCaptureSession(private val cameraManager: CameraManager): Closeable {
+  companion object {
+    private const val TAG = "PersistentCameraCaptureSession"
+  }
+  // Inputs/Dependencies
   private var repeatingRequest: RepeatingRequest? = null
   private var outputs: List<SurfaceOutput> = emptyList()
+  private var cameraId: String? = null
+  private var isActive = false
 
-  private val cameraDeviceDetails = CameraDeviceDetails(cameraManager, device.id)
+  // State/Dependants
+  private var session: CameraCaptureSession? = null
+  private var device: CameraDevice? = null
+  private var cameraDeviceDetails: CameraDeviceDetails? = null
+    get() {
+      val device = device ?: return null
+      if (field == null || field?.cameraId != device.id) {
+        field = CameraDeviceDetails(cameraManager, device.id)
+      }
+      return field
+    }
 
-  private suspend fun createSession(outputs: List<SurfaceOutput>): CameraCaptureSession {
+  private suspend fun createDevice(cameraId: String): CameraDevice {
+    return cameraManager.openCamera(cameraId, { device, error ->
+      if (this.device == device) {
+        this.device = null
+      }
+      if (error != null) {
+        Log.e(TAG, "Camera #$cameraId has been disconnected!", error)
+      }
+    }, CameraQueues.cameraQueue)
+  }
+
+  private suspend fun createSession(device: CameraDevice, outputs: List<SurfaceOutput>): CameraCaptureSession {
     if (outputs.isEmpty()) {
       throw Error("Cannot configure PersistentCameraCaptureSession without outputs!")
     }
@@ -36,28 +63,59 @@ class PersistentCameraCaptureSession(private val cameraManager: CameraManager,
   private fun updateRepeatingRequest() {
     val request = repeatingRequest ?: return
     val session = session ?: return
+    val device = device ?: return
+    val cameraDeviceDetails = cameraDeviceDetails ?: return
+    if (!isActive) {
+      return
+    }
 
     val repeatingRequest = request.toRepeatingRequest(device, cameraDeviceDetails)
     session.setRepeatingRequest(repeatingRequest, null, null)
   }
 
+  suspend fun setInput(cameraId: String) {
+    if (this.cameraId != cameraId) {
+      // Set target input values
+      this.cameraId = cameraId
+
+      if (device?.id != cameraId) {
+        // Close everything that depends on that device
+        session?.abortCaptures()
+        session = null
+        device?.close() // <-- this will close the session as well
+
+        // Create a new device
+        device = createDevice(cameraId)
+      }
+    }
+  }
+
   suspend fun setOutputs(outputs: List<SurfaceOutput>) {
-    if (outputs != this.outputs && outputs.isNotEmpty()) {
-      val session = createSession(outputs)
-      this.session = session
+    if (this.outputs != outputs) {
+      // Set target input values
       this.outputs = outputs
-      updateRepeatingRequest()
+
+      val device = device
+      if (outputs.isNotEmpty() && device != null) {
+        // Update output session
+        session?.abortCaptures()
+        session = null
+        session = createSession(device, outputs)
+        updateRepeatingRequest()
+      }
     }
   }
 
   fun startRepeating(request: RepeatingRequest) {
-    if (request != this.repeatingRequest) {
+    if (this.repeatingRequest != request) {
       this.repeatingRequest = request
+      this.isActive = true
       updateRepeatingRequest()
     }
   }
 
   fun stopRepeating() {
+    this.isActive = false
     session?.stopRepeating()
   }
 
