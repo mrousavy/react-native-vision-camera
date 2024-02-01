2784d29b423f7f624e75312f3e6578a527124077
Marc Rousavy <me@mrousavy.com>
2024-02-01 17:49:54 +0100

    Use StateFlow<T>

--- package/android/src/main/java/com/mrousavy/camera/core/PersistentCameraCaptureSession.kt
+++ package/android/src/main/java/com/mrousavy/camera/core/PersistentCameraCaptureSession.kt
@@ -6,44 +6,92 @@ import android.hardware.camera2.CameraManager
 import android.hardware.camera2.CaptureRequest
 import android.hardware.camera2.TotalCaptureResult
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import com.mrousavy.camera.core.outputs.SurfaceOutput
 import com.mrousavy.camera.extensions.capture
 import com.mrousavy.camera.extensions.createCaptureSession
-import com.mrousavy.camera.extensions.isValid
 import com.mrousavy.camera.extensions.openCamera
 import com.mrousavy.camera.extensions.tryAbortCaptures
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import java.io.Closeable
+import java.util.concurrent.CancellationException
+import kotlin.coroutines.CoroutineContext
 
 /**
  * A [CameraCaptureSession] wrapper that safely handles interruptions and remains open whenever available.
+ *
+ * It uses Kotlin coroutines and Flow to connect async dependencies.
  */
-class PersistentCameraCaptureSession(private val cameraManager: CameraManager, private val callback: Callback) : Closeable {
+class PersistentCameraCaptureSession(private val cameraManager: CameraManager,
+                                     private val callback: Callback,
+                                     private val coroutineContext: CoroutineContext) : Closeable {
   companion object {
-    private const val TAG = "PersistentCameraCaptureSession"
+    private var counter = 1
   }
+  private val TAG = "PersistentCameraCaptureSession${counter++}"
+
+  private val coroutineScope = CoroutineScope(coroutineContext)
+
+  // State
+  private var isActive = MutableStateFlow(false)
+  private var repeatingRequest = MutableStateFlow<RepeatingRequest?>(null)
 
   // Inputs/Dependencies
-  private var repeatingRequest: RepeatingRequest? = null
-  private var surfaceOutputs: List<SurfaceOutput> = emptyList()
-  private var cameraId: String? = null
-  private var isActive = false
+  private var cameraId = MutableStateFlow<String?>(null)
+  private var outputs = MutableStateFlow(emptyList<SurfaceOutput>())
 
-  // State/Dependants
-  private var session: CameraCaptureSession? = null
-  private var device: CameraDevice? = null
-  private var cameraDeviceDetails: CameraDeviceDetails? = null
-    get() {
-      val device = device ?: return null
-      if (field == null || field?.cameraId != device.id) {
-        field = CameraDeviceDetails(cameraManager, device.id)
+  init {
+    coroutineScope.launch {
+      val deviceFlow = cameraId.filterNotNull().map { createDevice(it) }
+      val sessionFlow = combine(deviceFlow, outputs) { device, outputs -> createSession(device, outputs) }
+      sessionFlow.collect { session ->
       }
-      return field
     }
 
-  val outputs: List<SurfaceOutput>
-    get() = surfaceOutputs
-  val isRunning: Boolean
-    get() = isActive && session != null && device != null
+    coroutineScope.launch {
+      flowingCameraId.collect { newValue ->
+        cameraId = newValue
+      }
+      flowingOutputs.collect { newValue ->
+        outputs = newValue
+      }
+      flowingDeviceDetails.collect { newValue ->
+        cameraDeviceDetails = newValue
+      }
+      flowingDevice.collect { newValue ->
+        if (device != newValue) {
+          session?.abortCaptures()
+          device?.close()
+          device = newValue
+        }
+      }
+      flowingSession.collect { newValue ->
+        if (session != newValue) {
+          session?.abortCaptures()
+          session = newValue
+          updateRepeatingRequest()
+        }
+      }
+      flowingTrigger.collect { newValue ->
+        Log.i(TAG, "Flowing Trigger DID change")
+      }
+    }
+  }
+
+  private fun createRepeatingRequest(request: RepeatingRequest, device: CameraDevice?, deviceDetails: CameraDeviceDetails, outputs: List<SurfaceOutput>): CaptureRequest? {
+    if (device == null) return null
+    return request.toRepeatingRequest(device, deviceDetails, outputs)
+  }
 
   private suspend fun createDevice(cameraId: String): CameraDevice =
     cameraManager.openCamera(cameraId, { device, error ->
@@ -58,11 +106,13 @@ class PersistentCameraCaptureSession(private val cameraManager: CameraManager, p
       }
     }, CameraQueues.cameraQueue)
 
-  private suspend fun createSession(device: CameraDevice, outputs: List<SurfaceOutput>): CameraCaptureSession {
-    if (outputs.isEmpty()) {
-      throw Error("Cannot configure PersistentCameraCaptureSession without outputs!")
+  private suspend fun createSession(device: CameraDevice?, outputs: List<SurfaceOutput>): CameraCaptureSession? {
+    if (device == null) {
+      return null
     }
-
+    if (outputs.isEmpty()) {
+      return null
+    }
     return device.createCaptureSession(cameraManager, outputs, { session ->
       if (this.session == session) {
         this.session?.tryAbortCaptures()
@@ -87,21 +137,7 @@ class PersistentCameraCaptureSession(private val cameraManager: CameraManager, p
   }
 
   suspend fun setInput(cameraId: String) {
-    if (this.cameraId != cameraId) {
-      // Set target input values
-      this.cameraId = cameraId
-
-      if (device?.id != cameraId || device?.isValid != true) {
-        // Close everything that depends on that device
-        session?.abortCaptures()
-        session = null
-        device?.close() // <-- this will close the session as well
-
-        // Create a new device
-        device = createDevice(cameraId)
-        Log.i(TAG, "Device updated! ${device?.id}")
-      }
-    }
+    flowingCameraId = flowOf(cameraId)
   }
 
   suspend fun setOutputs(outputs: List<SurfaceOutput>) {
@@ -144,6 +180,7 @@ class PersistentCameraCaptureSession(private val cameraManager: CameraManager, p
   }
 
   override fun close() {
+    coroutineContext.cancel(CancellationException("Session is torn down."))
     session?.abortCaptures()
     device?.close()
   }
