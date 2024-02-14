b662eedc452c182515b4e444a77774ce65028e40
Marc Rousavy <me@mrousavy.com>
2024-02-14 15:21:34 +0100

    Parse all States

--- package/android/src/main/java/com/mrousavy/camera/extensions/CameraCaptureSession+setRepeatingRequestAndWaitForPrecapture.kt
+++ package/android/src/main/java/com/mrousavy/camera/extensions/CameraCaptureSession+setRepeatingRequestAndWaitForPrecapture.kt
@@ -26,36 +26,84 @@ enum class PrecaptureTrigger {
   AWB
 }
 
+enum class AutoMode {
+  Active,
+  Passive
+}
+
 enum class FocusState {
+  Inactive,
+  Scanning,
   Focused,
   NotFocused;
 
+  val isCompleted: Boolean
+    get() = this == Focused || this == NotFocused
+
   companion object {
-    fun fromAFState(aeState: Int): FocusState? {
-      return when (aeState) {
+    fun fromAFState(afState: Int): FocusState {
+      return when (afState) {
+        CaptureResult.CONTROL_AF_STATE_INACTIVE -> Inactive
+        CaptureResult.CONTROL_AF_STATE_ACTIVE_SCAN -> Scanning
         CaptureResult.CONTROL_AF_STATE_FOCUSED_LOCKED -> Focused
         CaptureResult.CONTROL_AF_STATE_NOT_FOCUSED_LOCKED -> NotFocused
-        else -> null
+        CaptureResult.CONTROL_AF_STATE_PASSIVE_SCAN -> Scanning
+        CaptureResult.CONTROL_AF_STATE_PASSIVE_FOCUSED -> Focused
+        CaptureResult.CONTROL_AF_STATE_PASSIVE_UNFOCUSED -> NotFocused
+        else -> throw Error("Invalid CONTROL_AF_STATE! $afState")
       }
     }
   }
 }
 enum class ExposureState {
+  Locked,
+  Inactive,
+  Precapture,
+  Searching,
   Converged,
   FlashRequired;
 
+  val isCompleted: Boolean
+    get() = this == Converged || this == FlashRequired
+
   companion object {
-    fun fromAEState(aeState: Int): ExposureState? {
+    fun fromAEState(aeState: Int): ExposureState {
       return when (aeState) {
+        CaptureResult.CONTROL_AE_STATE_INACTIVE -> Inactive
+        CaptureResult.CONTROL_AE_STATE_SEARCHING -> Searching
+        CaptureResult.CONTROL_AE_STATE_PRECAPTURE -> Precapture
         CaptureResult.CONTROL_AE_STATE_CONVERGED -> Converged
         CaptureResult.CONTROL_AE_STATE_FLASH_REQUIRED -> FlashRequired
-        else -> null
+        CaptureResult.CONTROL_AE_STATE_LOCKED -> Locked
+        else -> throw Error("Invalid CONTROL_AE_STATE! $aeState")
       }
     }
   }
 }
 
-data class ResultState(val focusState: FocusState?, val exposureState: ExposureState?)
+enum class WhiteBalanceState {
+  Inactive,
+  Locked,
+  Searching,
+  Converged;
+
+  val isCompleted: Boolean
+    get() = this == Converged
+
+  companion object {
+    fun fromAWBState(awbState: Int): WhiteBalanceState {
+      return when (awbState) {
+        CaptureResult.CONTROL_AWB_STATE_INACTIVE -> Inactive
+        CaptureResult.CONTROL_AWB_STATE_SEARCHING -> Searching
+        CaptureResult.CONTROL_AWB_STATE_CONVERGED -> Converged
+        CaptureResult.CONTROL_AWB_STATE_LOCKED -> Locked
+        else -> throw Error("Invalid CONTROL_AWB_STATE! $awbState")
+      }
+    }
+  }
+}
+
+data class ResultState(val focusState: FocusState, val exposureState: ExposureState, val whiteBalanceState: WhiteBalanceState)
 
 /**
  * Set a new repeating request for the [CameraCaptureSession] that contains a precapture trigger, and wait until the given precaptures have locked.
@@ -67,8 +115,6 @@ suspend fun CameraCaptureSession.setRepeatingRequestAndWaitForPrecapture(
   suspendCancellableCoroutine { continuation ->
     // Map<PrecaptureTrigger, Boolean> of all completed precaptures
     val completed = precaptureTriggers.associateWith { false }.toMutableMap()
-    var focusState: FocusState? = null
-    var exposureState: ExposureState? = null
 
     CoroutineScope(Dispatchers.Default).launch {
       delay(5000) // after 5s, cancel capture
@@ -92,38 +138,40 @@ suspend fun CameraCaptureSession.setRepeatingRequestAndWaitForPrecapture(
 
           if (continuation.isActive) {
             // AF Precapture
+            val afState = FocusState.fromAFState(result.get(CaptureResult.CONTROL_AF_STATE) ?: CaptureResult.CONTROL_AF_STATE_INACTIVE)
+            val aeState = ExposureState.fromAEState(result.get(CaptureResult.CONTROL_AE_STATE) ?: CaptureResult.CONTROL_AE_STATE_INACTIVE)
+            val awbState = WhiteBalanceState.fromAWBState(result.get(CaptureResult.CONTROL_AWB_STATE) ?: CaptureResult.CONTROL_AWB_STATE_INACTIVE)
+
             if (precaptureTriggers.contains(PrecaptureTrigger.AF) && completed[PrecaptureTrigger.AF] != true) {
-              val afState = result.get(CaptureResult.CONTROL_AF_STATE)
-              Log.i(TAG, "AF State: $afState")
-              if (afState == CaptureResult.CONTROL_AF_STATE_FOCUSED_LOCKED || afState == CaptureResult.CONTROL_AF_STATE_NOT_FOCUSED_LOCKED) {
-                focusState = FocusState.fromAFState(afState)
+              if (afState.isCompleted) {
                 completed[PrecaptureTrigger.AF] = true
-                Log.i(TAG, "AF precapture completed! State: $focusState")
+                Log.i(TAG, "AF precapture completed! State: $afState")
+              } else {
+                Log.i(TAG, "AF State: $afState")
               }
             }
             // AE Precapture
             if (precaptureTriggers.contains(PrecaptureTrigger.AE) && completed[PrecaptureTrigger.AE] != true) {
-              val aeState = result.get(CaptureResult.CONTROL_AE_STATE)
-              Log.i(TAG, "AE State: $aeState")
-              if (aeState == CaptureResult.CONTROL_AE_STATE_CONVERGED || aeState == CaptureResult.CONTROL_AE_STATE_FLASH_REQUIRED) {
-                exposureState = ExposureState.fromAEState(aeState)
+              if (aeState.isCompleted) {
                 completed[PrecaptureTrigger.AE] = true
-                Log.i(TAG, "AE precapture completed! State: $exposureState")
+                Log.i(TAG, "AE precapture completed! State: $aeState")
+              } else {
+                Log.i(TAG, "AE State: $aeState")
               }
             }
             // AWB Precapture
             if (precaptureTriggers.contains(PrecaptureTrigger.AWB) && completed[PrecaptureTrigger.AWB] != true) {
-              val aeState = result.get(CaptureResult.CONTROL_AWB_STATE)
-              Log.i(TAG, "AWB State: $aeState")
-              if (aeState == CaptureResult.CONTROL_AWB_STATE_CONVERGED) {
+              if (awbState.isCompleted) {
                 completed[PrecaptureTrigger.AWB] = true
-                Log.i(TAG, "AWB precapture completed!")
+                Log.i(TAG, "AWB precapture completed! State: $awbState")
+              } else {
+                Log.i(TAG, "AWB State: $awbState")
               }
             }
 
             if (completed.values.all { it == true }) {
               // All precaptures did complete!
-              continuation.resume(ResultState(focusState, exposureState))
+              continuation.resume(ResultState(afState, aeState, awbState))
               session.setRepeatingRequest(request, null, null)
             }
           }
