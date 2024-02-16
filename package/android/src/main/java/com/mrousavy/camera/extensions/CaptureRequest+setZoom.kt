be9a9dea6003d8540359fe50221d0b92717430c4
Marc Rousavy <me@mrousavy.com>
2024-02-16 10:22:47 +0100

    Refactor zoom a bit

--- package/android/src/main/java/com/mrousavy/camera/extensions/CaptureRequest+setZoom.kt
+++ package/android/src/main/java/com/mrousavy/camera/extensions/CaptureRequest+setZoom.kt
@@ -6,14 +6,13 @@ import com.mrousavy.camera.core.CameraDeviceDetails
 import com.mrousavy.camera.types.HardwareLevel
 
 fun CaptureRequest.Builder.setZoom(zoom: Float, deviceDetails: CameraDeviceDetails) {
+  val zoomRange = deviceDetails.zoomRange
+  val zoomClamped = zoomRange.clamp(zoom)
+
   if (deviceDetails.hardwareLevel.isAtLeast(HardwareLevel.LIMITED) && Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-    val zoomRange = deviceDetails.zoomRange
-    val zoomClamped = zoomRange.clamp(zoom)
     this.set(CaptureRequest.CONTROL_ZOOM_RATIO, zoomClamped)
   } else {
     val size = deviceDetails.activeSize
-    val zoomRange = deviceDetails.zoomRange
-    val zoomClamped = zoomRange.clamp(zoom)
     this.set(CaptureRequest.SCALER_CROP_REGION, size.zoomed(zoomClamped))
   }
 }
