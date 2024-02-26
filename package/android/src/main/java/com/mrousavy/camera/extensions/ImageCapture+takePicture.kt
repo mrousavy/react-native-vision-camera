7b463904bd1c4bd6b87fbb8b68c4615834d4d3b1
Marc Rousavy <me@mrousavy.com>
2024-02-26 20:48:30 +0100

    feat: Shutter sound and stabilization

--- package/android/src/main/java/com/mrousavy/camera/extensions/ImageCapture+takePicture.kt
+++ package/android/src/main/java/com/mrousavy/camera/extensions/ImageCapture+takePicture.kt
@@ -1,5 +1,6 @@
 package com.mrousavy.camera.extensions
 
+import android.media.MediaActionSound
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.ImageProxy
@@ -8,11 +9,21 @@ import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlinx.coroutines.suspendCancellableCoroutine
 
-suspend inline fun ImageCapture.takePicture(executor: Executor): ImageProxy =
+suspend inline fun ImageCapture.takePicture(enableShutterSound: Boolean, executor: Executor): ImageProxy =
   suspendCancellableCoroutine { continuation ->
+    val shutterSound = if (enableShutterSound) MediaActionSound() else null
+    shutterSound?.load(MediaActionSound.SHUTTER_CLICK)
+
     takePicture(
       executor,
       object : ImageCapture.OnImageCapturedCallback() {
+        override fun onCaptureStarted() {
+          super.onCaptureStarted()
+          if (enableShutterSound) {
+            shutterSound?.play(MediaActionSound.SHUTTER_CLICK)
+          }
+        }
+
         override fun onCaptureSuccess(image: ImageProxy) {
           super.onCaptureSuccess(image)
           if (continuation.isActive) {
