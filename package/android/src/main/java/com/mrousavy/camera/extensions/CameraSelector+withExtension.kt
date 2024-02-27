22064db98e71a5c33b7fcb727e00f2a1432f1699
Marc Rousavy <me@mrousavy.com>
2024-02-27 10:22:39 +0100

    Better `mainExecutor`

--- package/android/src/main/java/com/mrousavy/camera/extensions/CameraSelector+withExtension.kt
+++ package/android/src/main/java/com/mrousavy/camera/extensions/CameraSelector+withExtension.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.camera.core.CameraSelector
 import androidx.camera.extensions.ExtensionsManager
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 
 private const val TAG = "CameraSelector"
 
@@ -16,13 +17,17 @@ suspend fun CameraSelector.withExtension(
   extensionDebugName: String
 ): CameraSelector {
   Log.i(TAG, "$extensionDebugName is enabled, looking up vendor $extensionDebugName extension...")
-  val extensionsManager = ExtensionsManager.getInstanceAsync(context, provider).await()
+
+  val mainExecutor = ContextCompat.getMainExecutor(context)
+  val extensionsManager = ExtensionsManager.getInstanceAsync(context, provider).await(mainExecutor)
+
   if (extensionsManager.isExtensionAvailable(this, extension)) {
     if (needsImageAnalysis && !extensionsManager.isImageAnalysisSupported(this, extension)) {
       Log.i(TAG, "Device supports a $extensionDebugName vendor extension, but we cannot use it since we need ImageAnalysis " +
           "and this extension does not work with ImageAnalysis use-cases.")
       return this
     }
+
     Log.i(TAG, "Device supports a $extensionDebugName vendor extension! Enabling...")
     return extensionsManager.getExtensionEnabledCameraSelector(this, extension)
   }
