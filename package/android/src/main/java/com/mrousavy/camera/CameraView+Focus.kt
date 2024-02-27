6d2952b818ebe186de9e837840e6ca5d9fe4c801
Marc Rousavy <me@mrousavy.com>
2024-02-27 10:28:43 +0100

    Implement `focus()`

--- package/android/src/main/java/com/mrousavy/camera/CameraView+Focus.kt
+++ package/android/src/main/java/com/mrousavy/camera/CameraView+Focus.kt
@@ -3,7 +3,9 @@ package com.mrousavy.camera
 import com.facebook.react.bridge.ReadableMap
 
 suspend fun CameraView.focus(pointMap: ReadableMap) {
-  val x = pointMap.getInt("x")
-  val y = pointMap.getInt("y")
-  cameraSession.focus(x, y)
+  val x = pointMap.getDouble("x")
+  val y = pointMap.getDouble("y")
+
+  val point = previewView.meteringPointFactory.createPoint(x.toFloat(), y.toFloat())
+  cameraSession.focus(point)
 }
