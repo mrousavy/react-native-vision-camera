0f85d246d62ccca8046be4de7df80c6263d60d55
Marc Rousavy <me@mrousavy.com>
2024-03-20 11:17:51 +0100

    format

--- package/android/src/main/java/com/mrousavy/camera/extensions/Float.kt
+++ package/android/src/main/java/com/mrousavy/camera/extensions/Float.kt
@@ -3,4 +3,4 @@ package com.mrousavy.camera.extensions
 import android.content.res.Resources
 
 val Float.px: Float
-    get() = this * Resources.getSystem().displayMetrics.density
+  get() = this * Resources.getSystem().displayMetrics.density
