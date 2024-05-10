94371764c73a4b6791942cf68d2f17bac0bfc727
Marc Rousavy <me@mrousavy.com>
2024-05-10 11:40:30 +0200

    feat: Hand detection example

--- package/example/ios/Frame Processor Plugins/Example Swift Plugin/ExampleSwiftFrameProcessor.swift
+++ package/example/ios/Frame Processor Plugins/Example Swift Plugin/ExampleSwiftFrameProcessor.swift
@@ -7,42 +7,63 @@
 
 #if VISION_CAMERA_ENABLE_FRAME_PROCESSORS
 import VisionCamera
+import MediaPipeTasksVision
 
 // Example for a Swift Frame Processor plugin
 @objc(ExampleSwiftFrameProcessorPlugin)
 public class ExampleSwiftFrameProcessorPlugin: FrameProcessorPlugin {
+  private let handLandmarker: HandLandmarker
+  
   public override init(proxy: VisionCameraProxyHolder, options: [AnyHashable: Any]! = [:]) {
-    super.init(proxy: proxy, options: options)
 
-    print("ExampleSwiftFrameProcessorPlugin initialized with options: \(String(describing: options))")
+    guard let modelPath = Bundle.main.path(forResource: "hand_landmarker",
+                                           ofType: "task") else {
+      fatalError("Model not found!")
+    }
+
+    let landmarkerOptions = HandLandmarkerOptions()
+    landmarkerOptions.baseOptions.modelAssetPath = modelPath
+    landmarkerOptions.runningMode = .liveStream
+    landmarkerOptions.minHandDetectionConfidence = 0.6
+    landmarkerOptions.minHandPresenceConfidence = 0.6
+    landmarkerOptions.minTrackingConfidence = 0.6
+    landmarkerOptions.numHands = 2
+
+    guard let handLandmarker = try? HandLandmarker(options: landmarkerOptions) else {
+      fatalError("Failed to init Hand Landmarker!")
+    }
+    self.handLandmarker = handLandmarker
+    super.init(proxy: proxy, options: options)
   }
 
   public override func callback(_ frame: Frame, withArguments arguments: [AnyHashable: Any]?) -> Any? {
-    let imageBuffer = CMSampleBufferGetImageBuffer(frame.buffer)
-
-    if let arguments, let imageBuffer {
-      let width = CVPixelBufferGetWidth(imageBuffer)
-      let height = CVPixelBufferGetHeight(imageBuffer)
-      let count = arguments.count
-
-      print(
-        "ExampleSwiftPlugin: \(width) x \(height) Image. Logging \(count) parameters:"
-      )
-
-      for key in arguments.keys {
-        let value = arguments[key]
-        let valueString = String(describing: value)
-        let valueClassString = String(describing: value.self)
-        print("ExampleSwiftPlugin:   -> \(valueString) (\(valueClassString))")
-      }
+    guard let image = try? MPImage(sampleBuffer: frame.buffer) else {
+      return []
     }
-
-    return [
-      "example_str": "SwiftTest",
-      "example_bool": false,
-      "example_double": 6.7,
-      "example_array": ["Good bye", false, 21.37]
-    ]
+    guard let results = try? handLandmarker.detect(image: image) else {
+      return []
+    }
+    
+    var hands: [[String: Any]] = []
+    for i in 0..<results.handedness.count {
+      hands.append([
+        "landmarks": results.landmarks[i].map({ landmark in
+          return [
+            "x": NSNumber(value: landmark.x),
+            "y": NSNumber(value: landmark.y),
+            "z": NSNumber(value: landmark.z),
+            "visibility": landmark.visibility,
+            "presence": landmark.presence
+          ]
+        }),
+        "handedness": results.handedness[i].map({ re in
+          return [
+            "score": re.score,
+          ]
+        })
+      ])
+    }
+    return hands
   }
 }
 #endif
