9d947bbbb49a9144d69349099a1755b779ce5df7
Marc Rousavy <me@mrousavy.com>
2024-06-08 16:39:38 +0200

    Create CMTime+invert.swift

---
+++ package/ios/Core/Extensions/CMTime+invert.swift
@@ -0,0 +1,19 @@
+//
+//  CMTime+invert.swift
+//  VisionCamera
+//
+//  Created by Marc Rousavy on 08.06.24.
+//
+
+import Foundation
+import CoreMedia
+
+extension CMTime {
+  /**
+   Inverts the time.
+   e.g. 3.5 seconds -> -3.5 seconds
+   */
+  func invert() -> CMTime {
+    return CMTime(value: value * -1, timescale: timescale)
+  }
+}
