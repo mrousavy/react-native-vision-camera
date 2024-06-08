36bb679dae1273328ee0a641b99781bdc49f09de
Marc Rousavy <me@mrousavy.com>
2024-06-08 16:39:40 +0200

    Create CMSampleBuffer+copyWithTimestampOffset.swift

---
+++ package/ios/Core/Extensions/CMSampleBuffer+copyWithTimestampOffset.swift
@@ -0,0 +1,64 @@
+//
+//  CMSampleBuffer+copyWithTimestampOffset.swift
+//  VisionCamera
+//
+//  Created by Marc Rousavy on 08.06.24.
+//
+
+import Foundation
+import CoreMedia
+
+enum TimestampAdjustmentError: Error {
+  case failedToCopySampleBuffer(status: OSStatus)
+  case sampleBufferCopyIsNil
+  case failedToGetTimingInfo(status: OSStatus)
+  case noTimingEntriesNeeded
+}
+
+private let kSampleBufferError_NoError: OSStatus = 0
+
+extension CMSampleBuffer {
+  
+  /**
+   Returns a copy of the current CMSampleBuffer with the timing info adjusted by the given offset.
+   The decode and presentation timestamps will be shifted by the given offset.
+   The actual pixel buffer will not be copied, only metadata information will.
+   */
+  func copyWithTimestampOffset(_ offset: CMTime) throws -> CMSampleBuffer {
+    let samplesCount = CMSampleBufferGetNumSamples(self)
+    let timingInfo = UnsafeMutablePointer<CMSampleTimingInfo>.allocate(capacity: samplesCount)
+    var entriesNeeded: Int = samplesCount
+    let getResult = CMSampleBufferGetSampleTimingInfoArray(self,
+                                                           entryCount: samplesCount,
+                                                           arrayToFill: timingInfo,
+                                                           entriesNeededOut: &entriesNeeded)
+    guard getResult == kSampleBufferError_NoError else {
+      // failed to get timing info
+      throw TimestampAdjustmentError.failedToGetTimingInfo(status: getResult)
+    }
+    guard entriesNeeded > 0 else {
+      // there are no entries in the timing info - we cannot adjust anything
+      throw TimestampAdjustmentError.noTimingEntriesNeeded
+    }
+    
+    for i in 0...samplesCount {
+      timingInfo[i].decodeTimeStamp = timingInfo[i].decodeTimeStamp + offset
+      timingInfo[i].presentationTimeStamp = timingInfo[i].presentationTimeStamp + offset
+    }
+    
+    var newBuffer: CMSampleBuffer?
+    let copyResult = CMSampleBufferCreateCopyWithNewTiming(allocator: nil,
+                                                           sampleBuffer: self,
+                                                           sampleTimingEntryCount: entriesNeeded,
+                                                           sampleTimingArray: timingInfo,
+                                                           sampleBufferOut: &newBuffer)
+    guard copyResult == kSampleBufferError_NoError else {
+      throw TimestampAdjustmentError.failedToCopySampleBuffer(status: copyResult)
+    }
+    guard let newBuffer else {
+      throw TimestampAdjustmentError.sampleBufferCopyIsNil
+    }
+    return newBuffer
+  }
+  
+}
