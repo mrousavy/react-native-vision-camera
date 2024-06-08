a0cc12b446e36d6548bb7cbc8379e6936da28f5e
Marc Rousavy <me@mrousavy.com>
2024-06-08 16:58:59 +0200

    fix: Fix adjusting timestamp

--- package/ios/Core/Extensions/CMSampleBuffer+copyWithTimestampOffset.swift
+++ package/ios/Core/Extensions/CMSampleBuffer+copyWithTimestampOffset.swift
@@ -14,46 +14,57 @@ enum TimestampAdjustmentError: Error {
   case failedToCopySampleBuffer(status: OSStatus)
   case sampleBufferCopyIsNil
   case failedToGetTimingInfo(status: OSStatus)
-  case noTimingEntriesNeeded
+  case noTimingEntriesFound
 }
 
 private let kSampleBufferError_NoError: OSStatus = 0
 
 extension CMSampleBuffer {
+  private func getTimingInfos() throws -> [CMSampleTimingInfo] {
+    var count: CMItemCount = 0
+    let getCountStatus = CMSampleBufferGetSampleTimingInfoArray(self,
+                                                                entryCount: 0,
+                                                                arrayToFill: nil,
+                                                                entriesNeededOut: &count)
+    guard getCountStatus == kSampleBufferError_NoError else {
+      throw TimestampAdjustmentError.failedToGetTimingInfo(status: getCountStatus)
+    }
+
+    let emptyTimingInfo = CMSampleTimingInfo(duration: .invalid,
+                                             presentationTimeStamp: .invalid,
+                                             decodeTimeStamp: .invalid)
+    var infos = [CMSampleTimingInfo](repeating: emptyTimingInfo, count: count)
+    let getArrayStatus = CMSampleBufferGetSampleTimingInfoArray(self,
+                                                                entryCount: count,
+                                                                arrayToFill: &infos,
+                                                                entriesNeededOut: nil)
+    guard getArrayStatus == kSampleBufferError_NoError else {
+      throw TimestampAdjustmentError.failedToGetTimingInfo(status: getArrayStatus)
+    }
+    guard !infos.isEmpty else {
+      throw TimestampAdjustmentError.noTimingEntriesFound
+    }
+    return infos
+  }
+
   /**
    Returns a copy of the current CMSampleBuffer with the timing info adjusted by the given offset.
    The decode and presentation timestamps will be shifted by the given offset.
    The actual pixel buffer will not be copied, only metadata information will.
    */
   func copyWithTimestampOffset(_ offset: CMTime) throws -> CMSampleBuffer {
-    let samplesCount = CMSampleBufferGetNumSamples(self)
-    let timingInfo = UnsafeMutablePointer<CMSampleTimingInfo>.allocate(capacity: samplesCount)
-    var entriesNeeded: Int = samplesCount
-    let getResult = CMSampleBufferGetSampleTimingInfoArray(self,
-                                                           entryCount: samplesCount,
-                                                           arrayToFill: timingInfo,
-                                                           entriesNeededOut: &entriesNeeded)
-    guard getResult == kSampleBufferError_NoError else {
-      // failed to get timing info
-      throw TimestampAdjustmentError.failedToGetTimingInfo(status: getResult)
-    }
-    guard entriesNeeded > 0 else {
-      // there are no entries in the timing info - we cannot adjust anything
-      throw TimestampAdjustmentError.noTimingEntriesNeeded
-    }
-
-    for i in 0 ... samplesCount {
-      // swiftlint:disable shorthand_operator
-      timingInfo[i].decodeTimeStamp = timingInfo[i].decodeTimeStamp + offset
-      timingInfo[i].presentationTimeStamp = timingInfo[i].presentationTimeStamp + offset
-      // swiftlint:enable shorthand_operator
+    let timingInfos = try getTimingInfos()
+    let newTimingInfos = timingInfos.map { timingInfo in
+      return CMSampleTimingInfo(duration: timingInfo.duration,
+                                presentationTimeStamp: timingInfo.presentationTimeStamp + offset,
+                                decodeTimeStamp: timingInfo.decodeTimeStamp + offset)
     }
 
     var newBuffer: CMSampleBuffer?
     let copyResult = CMSampleBufferCreateCopyWithNewTiming(allocator: nil,
                                                            sampleBuffer: self,
-                                                           sampleTimingEntryCount: entriesNeeded,
-                                                           sampleTimingArray: timingInfo,
+                                                           sampleTimingEntryCount: newTimingInfos.count,
+                                                           sampleTimingArray: newTimingInfos,
                                                            sampleBufferOut: &newBuffer)
     guard copyResult == kSampleBufferError_NoError else {
       throw TimestampAdjustmentError.failedToCopySampleBuffer(status: copyResult)
@@ -61,6 +72,7 @@ extension CMSampleBuffer {
     guard let newBuffer else {
       throw TimestampAdjustmentError.sampleBufferCopyIsNil
     }
+
     return newBuffer
   }
 }
