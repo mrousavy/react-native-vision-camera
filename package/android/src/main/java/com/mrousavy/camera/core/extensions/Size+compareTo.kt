f6d86449e56040dcee1bbad117800143ee4040fe
Marc Rousavy <me@mrousavy.com>
2024-06-25 16:42:06 +0200

    fix: Also use that selector now for Preview

--- package/android/src/main/java/com/mrousavy/camera/core/extensions/Size+compareTo.kt
+++
@@ -1,5 +0,0 @@
-package com.mrousavy.camera.core.extensions
-
-import android.util.Size
-
-operator fun Size.compareTo(other: Size): Int = (this.width * this.height).compareTo(other.width * other.height)
