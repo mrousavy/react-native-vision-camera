0ea5cbf1c42143c74eeef7dc0b65f326c05e9cd3
Marc Rousavy <me@mrousavy.com>
2024-07-02 12:49:00 +0200

    feat: Dynamically determine sensor orientation on iOS

--- package/ios/Core/Extensions/AVCaptureDevice+sensorOrientation.swift
+++ package/ios/Core/Extensions/AVCaptureDevice+sensorOrientation.swift
@@ -8,13 +8,20 @@
 import AVFoundation
 import Foundation
 
+// On iOS, all built-in Cameras are landscape-left (90deg rotated)
+private let DEFAULT_SENSOR_ORIENTATION = Orientation.landscapeLeft
+
 extension AVCaptureDevice {
   /**
    Get the natural orientation of the camera sensor of this specific device.
-
-   Note: On iOS, all sensors are landscape-left.
    */
   var sensorOrientation: Orientation {
-    return .landscapeLeft
+    guard #available(iOS 17.0, *) else {
+      return DEFAULT_SENSOR_ORIENTATION
+    }
+    
+    let rotationCoordinator = RotationCoordinator(device: self, previewLayer: nil)
+    let degrees = rotationCoordinator.videoRotationAngleForHorizonLevelCapture
+    return Orientation(degrees: degrees)
   }
 }
