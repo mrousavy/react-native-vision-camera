c6aba9fb16177256e859996ea8928adca27c3415
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-10-25 10:42:35 +0200

    TEMP: fixed issue where local linked RNWC wasn't working

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,11 +1,11 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config')
 const path = require('path')
-const escape = require('escape-string-regexp')
-const exclusionList = require('metro-config/src/defaults/exclusionList')
-const pak = require('../package.json')
 
 const root = path.resolve(__dirname, '..')
-const modules = Object.keys({ ...pak.peerDependencies })
+
+// TODO: remove this once RNWC is published
+const fs = require('fs');
+const rnwc = fs.realpathSync(path.resolve(root, 'node_modules', 'react-native-worklets-core'))
 
 /**
  * Metro configuration
@@ -14,23 +14,7 @@ const modules = Object.keys({ ...pak.peerDependencies })
  * @type {import('metro-config').MetroConfig}
  */
 const config = {
-  watchFolders: [root],
-
-  // We need to make sure that only one version is loaded for peerDependencies
-  // So we block them at the root, and alias them to the versions in example's node_modules
-  resolver: {
-    blacklistRE: exclusionList(
-      modules.map(
-        (m) =>
-          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
-      )
-    ),
-
-    extraNodeModules: modules.reduce((acc, name) => {
-      acc[name] = path.join(__dirname, 'node_modules', name)
-      return acc
-    }, {}),
-  },
+  watchFolders: [root, rnwc],
 
   transformer: {
     getTransformOptions: async () => ({
