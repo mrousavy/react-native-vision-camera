5fb1ed354d9128447b3380df16881cb384dfdd80
Hanno J. Gödecke <die.drei99@yahoo.de>
2024-10-28 16:49:29 +0100

    pragma once

--- package/ios/React/VisionCamera.h
+++ package/ios/React/VisionCamera.h
@@ -5,10 +5,6 @@
 //  Created by Hanno Gödecke on 22.10.24.
 //
 
-#ifndef VisionCamera_h
-#define VisionCamera_h
+#pragma once
 
 // Needed as a workaround for: https://github.com/CocoaPods/CocoaPods/issues/10544
-// TODO: do we need to import the umbrella header here?
-
-#endif /* VisionCamera_h */
