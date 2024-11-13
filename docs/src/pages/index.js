8c5d96af7eabed3721aaa7a81a22b8887422b095
Marc Rousavy <me@mrousavy.com>
2024-11-13 23:06:48 +0000

    docs: Add Vercel analytics

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -5,7 +5,6 @@ import Link from '@docusaurus/Link'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import useBaseUrl from '@docusaurus/useBaseUrl'
 import styles from './styles.module.css'
-import { Analytics } from '@vercel/analytics/react'
 
 const features = [
   {
@@ -87,8 +86,6 @@ function Home() {
             </div>
           </section>
         )}
-
-        <Analytics />
       </main>
     </Layout>
   )
