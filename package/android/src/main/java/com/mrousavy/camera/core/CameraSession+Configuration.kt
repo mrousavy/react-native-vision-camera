91f279d3b9951c6382b5d75b8498286de8e6ec80
ismaelsousa <ismael1998sousa@gmail.com>
2024-11-14 10:30:23 -0300

    docs: Update preview documentation and improve CameraSession configuration logic

--- package/android/src/main/java/com/mrousavy/camera/core/CameraSession+Configuration.kt
+++ package/android/src/main/java/com/mrousavy/camera/core/CameraSession+Configuration.kt
@@ -75,9 +75,11 @@ internal fun CameraSession.configureOutputs(configuration: CameraConfiguration) 
       }
 
       if (format != null) {
-        // Similar to iOS, Preview will follow video size as it's size (and aspect ratio)
+        // If video is disable but photo is enabled, use photoSize
+        val targetSize = if (videoConfig == null) format.photoSize else format.videoSize
+
         val previewResolutionSelector = ResolutionSelector.Builder()
-          .forSize(format.videoSize)
+          .forSize(targetSize)
           .setAllowedResolutionMode(ResolutionSelector.PREFER_CAPTURE_RATE_OVER_HIGHER_RESOLUTION)
           .build()
         preview.setResolutionSelector(previewResolutionSelector)
