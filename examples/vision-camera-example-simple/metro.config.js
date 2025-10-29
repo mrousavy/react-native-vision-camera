0caae9a7d570f6822cd434960740bae19476bed0
Marc Rousavy <me@mrousavy.com>
2025-10-29 14:42:45 +0100

    fix: Watch root folder to support monorepo

--- examples/vision-camera-example-simple/metro.config.js
+++ examples/vision-camera-example-simple/metro.config.js
@@ -1,11 +1,16 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config')
+const path = require('path')
+
+const root = path.resolve(__dirname, '..', '..')
 
 /**
  * Metro configuration
- * https://reactnative.dev/docs/metro
+ * https://facebook.github.io/metro/docs/configuration
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watchFolders: [root],
+}
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(getDefaultConfig(__dirname), config)
