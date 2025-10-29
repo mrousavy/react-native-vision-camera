350841079696550ee45295ae15ac72c68d7a0435
Marc Rousavy <me@mrousavy.com>
2025-10-29 14:05:51 +0100

    feat: Create example

---
+++ examples/vision-camera-example-simple/App.tsx
@@ -0,0 +1,31 @@
+import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
+import {
+  SafeAreaProvider,
+} from 'react-native-safe-area-context';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <AppContent />
+    </SafeAreaProvider>
+  );
+}
+
+function AppContent() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello world!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
