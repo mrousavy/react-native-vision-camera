84128db1401adbae49a08deed5be0f92abcfe685
Marc Rousavy <me@mrousavy.com>
2025-11-09 02:15:06 -0800

    fix: Use media types again

--- packages/react-native-vision-camera/ios/Extensions/AVCaptureConnection+init.swift
+++ packages/react-native-vision-camera/ios/Extensions/AVCaptureConnection+init.swift
@@ -10,21 +10,19 @@ import AVFoundation
 
 extension AVCaptureConnection {
   convenience init(input: AVCaptureDeviceInput, output: any HybridCameraOutputSpec) throws {
+    let mediaType = output.mediaType.toAVMediaType()
+    let targetPorts = input.ports.filter { $0.mediaType == mediaType }
+    guard !targetPorts.isEmpty else {
+      throw RuntimeError.error(withMessage: "Output \"\(output)\" operates on mediaType \"\(output.mediaType.stringValue)\", but Input \"\(input.device.uniqueID)\" does not have any ports for that mediaType!")
+    }
+    
     if let hybridOutput = output as? NativeCameraOutput {
       // a) It's a normal AVCaptureSessionOutput - connect it to all it's desired ports
-      let mediaType = output.mediaType.toAVMediaType()
-      let ports = input.ports(for: mediaType, sourceDeviceType: nil, sourceDevicePosition: .unspecified)
-      guard !ports.isEmpty else {
-        throw RuntimeError.error(withMessage: "Connection input \"\(input)\" does not have any ports with the requested media type \"\(output.mediaType.stringValue)\"!")
-      }
-      self.init(inputPorts: ports, output: hybridOutput.output)
+      self.init(inputPorts: targetPorts, output: hybridOutput.output)
     } else if let hybridPreview = output as? NativePreviewViewOutput {
       // b) It's a preview AVCapturePreviewLayer
-      let videoPort = input.ports.first { $0.mediaType == .video }
-      guard let videoPort else {
-        throw RuntimeError.error(withMessage: "Connection input \"\(input)\" does not have a video port!")
-      }
-      self.init(inputPort: videoPort, videoPreviewLayer: hybridPreview.previewLayer)
+      let port = targetPorts.first!
+      self.init(inputPort: port, videoPreviewLayer: hybridPreview.previewLayer)
     } else {
       // c) It's a bird? It's a plane? no idea
       throw RuntimeError.error(withMessage: "Connection output \"\(output)\" is not of type `NativeCameraOutput` or `NativePreviewViewOutput`!")
