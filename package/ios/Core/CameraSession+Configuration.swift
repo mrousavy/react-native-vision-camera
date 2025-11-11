eb07d6a2808a7fb1d3b15be849de4c4f6ceb450a
Doug Russell <doug@sitewire.co>
2025-11-11 15:58:16 -0500

    Detailed comment to justify cleaning up output delegate

--- package/ios/Core/CameraSession+Configuration.swift
+++ package/ios/Core/CameraSession+Configuration.swift
@@ -61,6 +61,15 @@ extension CameraSession {
 
     // Remove all outputs
     for output in captureSession.outputs {
+      // The following delegates are weak references
+      // so cleaning them up is not strictly necessary
+      // from a safety standpoint.
+      // It is good to mark that between this point
+      // and logic further down configureOutput()
+      // messages to delegate would be in an undefined
+      // state and with delegate == self the weak 
+      // references wouldn't be cleared during that 
+      // window.
       if let metadataOutput = output as? AVCaptureMetadataOutput {
         metadataOutput.setMetadataObjectsDelegate(nil, queue: nil)
       }
