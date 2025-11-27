c70b1762cafda82f193b9ab952f11e03adfc87ee
Laurids Kern <me@lauridskern.com>
2025-11-27 17:01:10 +0100

    Update fonts and theme colors in docs layout

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,14 +1,20 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
-import { Inter } from 'next/font/google';
+import { Geist, Geist_Mono } from 'next/font/google';
 
-const inter = Inter({
+const geist = Geist({
+  variable: '--font-sans',
+  subsets: ['latin'],
+});
+
+const mono = Geist_Mono({
+  variable: '--font-mono',
   subsets: ['latin'],
 });
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="en"   className={`${geist.variable} ${mono.variable}`} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
