672ef0eec2a0b6f98a32e601b556661065cfd7aa
Marc Rousavy <me@mrousavy.com>
2026-04-17 15:04:41 +0200

    Create CommonDynamicRanges.ts

---
+++ packages/react-native-vision-camera/src/utils/CommonDynamicRanges.ts
@@ -0,0 +1,23 @@
+import type { TargetDynamicRange } from '../specs/common-types/DynamicRange'
+
+/**
+ * Common Dynamic Ranges for the Camera.
+ */
+export const CommonDynamicRanges = {
+  /**
+   * Any SDR profile, preferrably 8-bit sRGB.
+   */
+  ANY_SDR: {
+    bitDepth: 'sdr-8-bit',
+    colorSpace: 'srgb',
+    colorRange: 'full',
+  } satisfies TargetDynamicRange,
+  /**
+   * Any HDR profile, preferrably 10-bit HLG_BT2020.
+   */
+  ANY_HDR: {
+    bitDepth: 'hdr-10-bit',
+    colorSpace: 'hlg-bt2020',
+    colorRange: 'full',
+  } satisfies TargetDynamicRange,
+}
