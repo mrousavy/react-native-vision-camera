e2084e91436033107f1762c705baec228e617f1f
Marc Rousavy <me@mrousavy.com>
2026-04-22 11:36:27 +0200

    link to margelo too

--- docs/src/lib/structured-data.ts
+++ docs/src/lib/structured-data.ts
@@ -5,28 +5,42 @@ const SITE_DESCRIPTION =
 
 const AUTHOR = {
   name: 'Marc Rousavy',
-  url: 'https://github.com/mrousavy',
-}
+  url: 'https://mrousavy.com',
+  sameAs: ['https://github.com/mrousavy', 'https://twitter.com/mrousavy'],
+} as const
+
+const COMPANY = {
+  name: 'Margelo',
+  url: 'https://margelo.com',
+  id: 'https://margelo.com/#organization',
+  sameAs: ['https://github.com/margelo', 'https://x.com/margelo_com'],
+} as const
 
 export function getSiteStructuredData() {
   const siteUrl = absoluteUrl('/')
   const logoUrl = absoluteUrl('/vc_icon.png')
+
+  const websiteId = `${siteUrl}#website`
+  const projectId = `${siteUrl}#organization`
+  const personId = `${siteUrl}#person`
+  const softwareId = `${siteUrl}#software`
 
   return {
     '@context': 'https://schema.org',
     '@graph': [
       {
         '@type': 'WebSite',
-        '@id': `${siteUrl}#website`,
+        '@id': websiteId,
         url: siteUrl,
         name: siteConfig.name,
         description: SITE_DESCRIPTION,
         inLanguage: 'en',
-        publisher: { '@id': `${siteUrl}#organization` },
+        publisher: { '@id': projectId },
+        about: { '@id': softwareId },
       },
       {
         '@type': 'Organization',
-        '@id': `${siteUrl}#organization`,
+        '@id': projectId,
         name: siteConfig.name,
         url: siteUrl,
         logo: {
@@ -36,25 +50,37 @@ export function getSiteStructuredData() {
           height: 1024,
         },
         sameAs: [siteConfig.repositoryUrl],
-        founder: { '@id': `${siteUrl}#person` },
+        founder: { '@id': personId },
+        parentOrganization: { '@id': COMPANY.id },
+      },
+      {
+        '@type': 'Organization',
+        '@id': COMPANY.id,
+        name: COMPANY.name,
+        url: COMPANY.url,
+        sameAs: [...COMPANY.sameAs],
+        founder: { '@id': personId },
       },
       {
         '@type': 'Person',
-        '@id': `${siteUrl}#person`,
+        '@id': personId,
         name: AUTHOR.name,
         url: AUTHOR.url,
+        sameAs: [...AUTHOR.sameAs],
+        worksFor: { '@id': COMPANY.id },
       },
       {
         '@type': 'SoftwareSourceCode',
-        '@id': `${siteUrl}#software`,
+        '@id': softwareId,
         name: 'react-native-vision-camera',
         description: SITE_DESCRIPTION,
         codeRepository: siteConfig.repositoryUrl,
         programmingLanguage: ['TypeScript', 'Swift', 'Kotlin', 'C++'],
         runtimePlatform: 'React Native',
         license: 'https://opensource.org/licenses/MIT',
-        author: { '@id': `${siteUrl}#person` },
         url: siteUrl,
+        author: { '@id': personId },
+        publisher: { '@id': projectId },
       },
     ],
   }
