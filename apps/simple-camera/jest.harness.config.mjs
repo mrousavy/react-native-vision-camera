1559772d924d150220c157ab61af4df0fd703378
Marc Rousavy <me@mrousavy.com>
2026-05-15 17:10:04 +0200

    remove all tests

--- apps/simple-camera/jest.harness.config.mjs
+++ apps/simple-camera/jest.harness.config.mjs
@@ -1,7 +1,8 @@
 const config = {
   preset: 'react-native-harness',
   testMatch: ['<rootDir>/__tests__/visioncamera.photo.harness.ts'],
-  testNamePattern: "captures at the device's maximum supported photo resolution",
+  testNamePattern:
+    "captures at the device's maximum supported photo resolution",
 }
 
 export default config
