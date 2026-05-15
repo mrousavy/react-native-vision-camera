c0190f30bbc142fe964da073327641595fae0a23
Marc Rousavy <me@mrousavy.com>
2026-05-15 17:43:34 +0200

    Revert "remove all tests"
    This reverts commit 1559772d924d150220c157ab61af4df0fd703378.

--- apps/simple-camera/jest.harness.config.mjs
+++ apps/simple-camera/jest.harness.config.mjs
@@ -1,8 +1,7 @@
 const config = {
   preset: 'react-native-harness',
   testMatch: ['<rootDir>/__tests__/visioncamera.photo.harness.ts'],
-  testNamePattern:
-    "captures at the device's maximum supported photo resolution",
+  testNamePattern: "captures at the device's maximum supported photo resolution",
 }
 
 export default config
