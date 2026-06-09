95de720a7f727f3cbf3fa41b99819fd9353efc57
Marc Rousavy <me@mrousavy.com>
2026-06-09 13:01:10 +0200

    docs: Add 96x96 favicon (#4010)

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -31,6 +31,7 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/favicon.ico', sizes: '32x32' },
+      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
     ],
